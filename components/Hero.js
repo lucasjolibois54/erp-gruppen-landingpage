63d443e6655e90cc4712d5de119a6579fcd526d9
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-03 11:51:52 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,13 @@
 function Hero() {
   return (
       <div>
-          <h1 className="text-4xl text-center max-w-2xl font-semibold">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
+          <h1 className="text-5xl text-center max-w-4xl font-semibold">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
+
+          <div className="mt-20 flex">
+          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} className="pt-10 pr-5 w-auto h-96" alt="mockup" />
+          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} className="pt-10 mt-20 pl-5 pr-5 w-auto h-96" alt="mockup" />
+          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} className="pt-10 pl-5 w-auto h-96" alt="mockup" />
+          </div>
       </div>
   ) 
   
