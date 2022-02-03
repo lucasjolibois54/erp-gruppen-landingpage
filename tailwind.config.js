2bc57505a6cfc895ff09c08ffacaa623a10a3a9b
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-03 11:20:08 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //background colors
+        'green-hl': '#204B45',
+
+      },},
   },
   plugins: [],
 }
