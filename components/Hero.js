07691a9dc1d5516fba9933902600cb6c23733ce7
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-03 12:57:49 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -2,15 +2,14 @@
 
 function Hero() {
   return (
-      <div className="text-center mx-auto">
-          <h1 className="text-5xl text-center max-w-4xl mx-auto font-semibold text-center">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
-
-          <div className="mt-20 flex flex-center space-x-10 pt-10">
-          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} className=" h-96" alt="mockup" />
-          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} className=" mt-20 h-96" alt="mockup" />
-          <img src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} className=" h-96" alt="mockup" />
-          </div>
+    <div>
+      <h1 className="text-5xl text-center max-w-4xl mx-auto font-semibold text-center">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
+      <div className="flex h-96 space-x-10 justify-center">
+          <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} alt="mockup" />
+          <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
+          <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} alt="mockup" />
       </div>
+    </div>
   ) 
   
 }
