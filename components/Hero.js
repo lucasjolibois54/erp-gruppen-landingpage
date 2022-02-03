e3d96f96f8b2c084d5274dccea9867bb7abb6ecb
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-03 13:02:28 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -3,10 +3,10 @@
 function Hero() {
   return (
     <div>
-      <h1 className="text-5xl text-center max-w-4xl mx-auto font-semibold text-center">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
-      <div className="flex h-96 space-x-10 justify-center">
+      <h1 className="text-5xl text-center max-w-4xl mx-auto font-semibold text-center mt-28">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
+      <div className="flex h-96 space-x-10 justify-center mt-20">
           <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} alt="mockup" />
-          <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
+          <img className="mt-20" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
           <img className="" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} alt="mockup" />
       </div>
     </div>
