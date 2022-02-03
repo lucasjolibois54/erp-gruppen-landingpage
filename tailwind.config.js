97bd930af93103e163e89ff73b534d19e8628762
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-03 14:56:56 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,30 +9,37 @@ module.exports = {
         //background colors
         'green-hl': '#204B45',
 
+      },
+      screens: {
+        'xxsm': '220px',
+        // => @media (min-width: 520px) { ... }
+      
+        'xsm': '370px',
+        // => @media (min-width: 520px) { ... }
+
+        'basesm': '450px',
+        // => @media (min-width: 520px) { ... }
+
+        'mdsm': '640px',
+        // => @media (min-width: 440px) { ... }
+      
+        'basemd': '740px',
+        // => @media (min-width: 768px) { ... }
+      
+        'baselg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+      
+        'base2lg': '1148px',
+        // => @media (min-width: 1148px) { ... }
+      
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
       },},
   },
-  screens: {
-    'xsm': '520px',
-    // => @media (min-width: 520px) { ... }
-  
-    'sm': '640px',
-    // => @media (min-width: 640px) { ... }
-  
-    'md': '768px',
-    // => @media (min-width: 768px) { ... }
-  
-    'lg': '1024px',
-    // => @media (min-width: 1024px) { ... }
-  
-    '2lg': '1148px',
-    // => @media (min-width: 1148px) { ... }
-  
-    'xl': '1280px',
-    // => @media (min-width: 1280px) { ... }
-  
-    '2xl': '1536px',
-    // => @media (min-width: 1536px) { ... }
-  },
+ 
     variants: {
       extend: {},
     },
