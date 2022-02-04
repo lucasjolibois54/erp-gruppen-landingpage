7b58386f0e9a6f43e4870870f584154ad2a67024
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-04 12:46:24 +0100

    Update Navigation.js

--- components/Navigation.js
+++ components/Navigation.js
@@ -56,98 +56,106 @@ const Navigation = ({ toggleMenu, setToggleMenu }) => {
     <>
       <AnimatePresence>
         {toggleMenu && (
-          <Nav>
-          <Container>
-            <NavHeader>
-              <Flex>
-                <h2></h2>
-                <CloseNav onClick={() => setToggleMenu(!toggleMenu)}>
-                  <button>
-                    <span></span>
-                    <span></span>
-                  </button>
-                </CloseNav>
-              </Flex>
-            </NavHeader>
-            <NavList>
-              <ul>
-                {navRoutes.map((route) => (
-                  <motion.li
-                    key={route.id}
-                    onHoverStart={() =>
-                      setRevealImage({
-                        show: true,
-                        image: route.image,
-                        key: route.id,
-                      })
-                    }
-                    onHoverEnd={() =>
-                      setRevealImage({
-                        show: false,
-                        image: route.image,
-                        key: route.id,
-                      })
-                    }
-                  >
-                    <Link href={`/projects/${route.path}`}>
-                      <motion.div
-                        initial={{ x: -108 }}
-                        whileHover={{
-                          x: -40,
-                          transition: {
-                            duration: 0.4,
-                            ease: [0.6, 0.05, -0.01, 0.9],
-                          },
-                        }}
-                        className="link"
-                      >
-                        <span className="arrow">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 101 57"
-                          >
-                            <path
-                              d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
-                              fill="#FFF"
-                              fillRule="evenodd"
-                            ></path>
-                          </svg>
-                        </span>
-                        {route.title}
-                      </motion.div>
-                    </Link>
-                  </motion.li>
-                ))}
-              </ul>
-            </NavList>
-            <NavFooter>Footer</NavFooter>
-            <NavImage>
-              <motion.div
-                animate={{ width: revealImage.show ? 0 : "100%" }}
-                className="reveal"
-              ></motion.div>
-              <div className="image">
-                <AnimatePresence initial={false} exitBeforeEnter>
-                  <motion.div
-                    key={revealImage.key}
-                    initial={{ opacity: 0 }}
-                    exit={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{
-                      duration: 0.2,
-                      ease: "easeInOut",
-                    }}
-                  >
-                    <Image
-                      src={require(`../image/${revealImage.image}`)}
-                      alt="Picture of the author"
-                    />
-                  </motion.div>
-                </AnimatePresence>
-              </div>
-            </NavImage>
-          </Container>
-        </Nav>
+          <Nav
+            initial={{ x: "-100%" }}
+            exit={{ x: "-100%" }}
+            animate={{ x: toggleMenu ? 0 : "-1" }}
+            transition={{
+              duration: .8,
+              ease: [ 0.6, 0.05, -0.01, 0.9]
+            }}
+          >
+            <Container>
+              <NavHeader>
+                <Flex>
+                  <h2></h2>
+                  <CloseNav onClick={() => setToggleMenu(!toggleMenu)}>
+                    <button>
+                      <span></span>
+                      <span></span>
+                    </button>
+                  </CloseNav>
+                </Flex>
+              </NavHeader>
+              <NavList>
+                <ul>
+                  {navRoutes.map((route) => (
+                    <motion.li
+                      key={route.id}
+                      onHoverStart={() =>
+                        setRevealImage({
+                          show: true,
+                          image: route.image,
+                          key: route.id,
+                        })
+                      }
+                      onHoverEnd={() =>
+                        setRevealImage({
+                          show: false,
+                          image: route.image,
+                          key: route.id,
+                        })
+                      }
+                    >
+                      <Link href={`/projects/${route.path}`}>
+                        <motion.div
+                          initial={{ x: -108 }}
+                          whileHover={{
+                            x: -40,
+                            transition: {
+                              duration: 0.4,
+                              ease: [0.6, 0.05, -0.01, 0.9],
+                            },
+                          }}
+                          className="link"
+                        >
+                          <span className="arrow">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 101 57"
+                            >
+                              <path
+                                d="M33 34H0V24h81.429L66 7.884 73.548 0l19.877 20.763.027-.029L101 28.618 73.829 57l-7.548-7.884L80.753 34H33z"
+                                fill="#FFF"
+                                fillRule="evenodd"
+                              ></path>
+                            </svg>
+                          </span>
+                          {route.title}
+                        </motion.div>
+                      </Link>
+                    </motion.li>
+                  ))}
+                </ul>
+              </NavList>
+              <NavFooter>Footer</NavFooter>
+              <NavImage>
+                <motion.div
+                  animate={{ width: revealImage.show ? 0 : "100%" }}
+                  className="reveal"
+                ></motion.div>
+                <div className="image">
+                  <AnimatePresence initial={false} exitBeforeEnter>
+                    <motion.div
+                      key={revealImage.key}
+                      initial={{ opacity: 0 }}
+                      exit={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                      transition={{
+                        duration: 0.2,
+                        ease: "easeInOut",
+                      }}
+                    >
+                      <Image
+                        src={require(`../image/${revealImage.image}`)}
+                        alt="Picture of the author"
+                      />
+                    </motion.div>
+                  </AnimatePresence>
+                </div>
+              </NavImage>
+            </Container>
+          </Nav>
         )}
       </AnimatePresence>
     </>
