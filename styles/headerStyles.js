5830f2b9547cbe65a5dd907815b28a4b867ea16f
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-07 13:38:01 +0100

    Update headerStyles.js

--- styles/headerStyles.js
+++ styles/headerStyles.js
@@ -4,11 +4,12 @@ import { motion } from "framer-motion"
 export const HeaderNav = styled(motion.div)`
   height: 0px;
   width: 100%;
-  position: absolute;
-  top: 72px;
+  position: relative;
+  top: 62px;
   right: 0;
   left: 0;
-  z-index: 99;
+  z-index: 101;
+  margin-bottom: 200px;
 `
 export const Logo = styled.div`
   a {
