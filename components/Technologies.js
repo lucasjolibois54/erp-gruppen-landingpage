f2b40487f11d9949bd8c9cc7da5e05a7874e379e
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-07 11:46:58 +0100

    Create Technologies.js

---
+++ components/Technologies.js
@@ -0,0 +1,43 @@
+function Technologies() {
+    return (
+        <div>
+            <div>
+                <h1 className="text-center max-w-6xl font-semibold mx-auto pt-40 xxsm:text-xl basemd:text-5xl ssm:text-4xl">Certificeret og erfarende konsulenter</h1>
+                <p className="text-center max-w-4xl mx-auto max-w-xl pt-10 xxsm:text-md ssm:text-xl">Our mission is to ensure teams can do their best work, no matter their size or budget. We focus on the details of everything we do.</p>
+            </div>
+
+            <div className='grid aboutlg:grid-cols-4 basemd:grid-cols-3 grid-cols-1 gap-4 text-center max-w-4xl mx-auto'>
+                <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png'} alt="mockup" />
+                    </div>
+                    
+
+
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png'} alt="mockup" />
+                    </div>
+                    <div>
+                <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-44 baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png'} alt="mockup" />
+                    </div>
+                    </div>
+        </div>
+    )
+
+}
+
+export default Technologies
