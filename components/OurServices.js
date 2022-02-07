44ba0d54403dff0411b0f92a6d638a177df13d83
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-02-07 14:46:35 +0100

    Create OurServices.js

---
+++ components/OurServices.js
@@ -0,0 +1,114 @@
+function OurServices() {
+    return (
+        <div>
+      <div>
+        <h1 className="text-center max-w-6xl font-semibold mx-auto pt-40 xxsm:text-xl basemd:text-5xl ssm:text-4xl">
+          Our Services
+        </h1>
+      </div>
+
+      <div className="grid aboutlg:grid-cols-4 ourservice:grid-cols-2 grid-cols-1 gap-4 text-center max-w-6xl mx-auto mb-20 mt-10">
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png"
+            }
+            alt="mockup"
+          />
+          <p className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div>
+          <img
+            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+            src={
+              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png"
+            }
+            alt="mockup"
+          />
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+      </div>
+    </div>
+    )
+
+}
+
+export default OurServices
