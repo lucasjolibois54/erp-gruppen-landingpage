4e2628993b8ed650123c8ade405b24089db0bc56
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-07 13:37:53 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,7 @@
 function Hero() {
   return (
     <div>
-      <h1 className="text-lg xxsm:text-base xsm:text-xl basesm:text-2xl mdsm:text-3xl basemd:text-4xl baselg:text-5xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-4 ssm:px-10 px-4 xsbasesm:px-16 text-center max-w-4xl mx-auto font-semibold text-center mt-28">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
+      <h1 className="text-lg xxsm:text-base xsm:text-xl basesm:text-2xl mdsm:text-3xl basemd:text-4xl baselg:text-5xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-4 ssm:px-10 px-4 xsbasesm:px-16 text-center max-w-4xl mx-auto font-medium text-center mt-28">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
       <div className="flex space-x-4 basemd:space-x-6 baselg:space-x-10 justify-center mt-8 mdsm:mt-16 basemd:mt-20">
         <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} alt="mockup" />
         <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto mt-6 lgbasemd:mt-10 base2lg:mt-20" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
