0815daa7e782c8bad91be55780455f30b60edb41
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-07 14:58:53 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -9,11 +9,6 @@ function Hero() {
         <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto mt-6 lgbasemd:mt-10 base2lg:mt-20" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
         <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} alt="mockup" />
       </div>
-
-      <div className="mt-48 mb-48">
-
-
-      </div>
     </div>
   )
 
