105c3967d47b1c00a240d54e5fe02937d8a0685e
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-02-07 10:34:53 +0100

    Update OurTeam.js

--- components/team/OurTeam.js
+++ components/team/OurTeam.js
@@ -3,76 +3,95 @@ import styles from './ourteam.module.css'
 function OurTeam() {
     return (
         <div className={styles.body}>
-              <div className='grid aboutlg:grid-cols-3 basemd:grid-cols-2 grid-cols-1 gap-4'>
-                  <div>
-            <div className={styles.card}>
-                <div className={styles.cardcontent}>
-                <h2 className={styles.cardtitle}>Annie Stanley</h2>
-                <p className={styles.cardbody}>Product Manager</p>
-                <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
-                <div className={styles.icons}>
-                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
-                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
-                </div>
-                </div>
-            </div>
-            </div>
-
-
-
             <div>
-            <div className={styles.card}>
-                <div className={styles.cardcontent}>
-                <h2 className={styles.cardtitle}>Annie Stanley</h2>
-                <p className={styles.cardbody}>Product Manager</p>
-                <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
-                <div className={styles.icons}>
-                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
-                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                <h3 className="text-center max-w-4xl font-semibold mx-auto pt-40 xxsm:text-xl ssm:text-3xl">Meet our team</h3>
+                <p className="text-center max-w-4xl mx-auto max-w-xl pt-10 xxsm:mb-20 xxsm:text-md ssm:text-xl">Our mission is to ensure teams can do their best work, no matter their size or budget. We focus on the details of everything we do.</p>
+            </div>
+            <div className='grid aboutlg:grid-cols-3 basemd:grid-cols-2 grid-cols-1 gap-4'>
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div className={styles.card}>
+                        <div className={styles.cardcontent}>
+                            <h2 className={styles.cardtitle}>Annie Stanley</h2>
+                            <p className={styles.cardbody}>Product Manager</p>
+                            <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
+                            <div className={styles.icons}>
+                                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
+                                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-
-
-
-
-
-            <div>
-            <div className={styles.card}>
-                <div className={styles.cardcontent}>
-                <h2 className={styles.cardtitle}>Annie Stanley</h2>
-                <p className={styles.cardbody}>Product Manager</p>
-                <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
-                <div className={styles.icons}>
-                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
-                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
-                </div>
-                </div>
-            </div>
-            </div>
-
-
-
-
-
-            <div>
-            <div className={styles.card}>
-                <div className={styles.cardcontent}>
-                <h2 className={styles.cardtitle}>Annie Stanley</h2>
-                <p className={styles.cardbody}>Product Manager</p>
-                <p className={styles.cardbody1}>Former PM for Airtable, Medium, Ghost, and Lumi.</p>
-                <div className={styles.icons}>
-                <img className={styles.twitter} href="#" src="twitter.svg" alt="Twitter logo" width="25" height="25"></img>
-                <img className={styles.linkedin} href="#" src="linkedin.svg" alt="linkedin logo" width="25" height="25"></img>
-                </div>
-                </div>
-            </div>
-            </div>
-
-
-
-</div>
         </div>
     )
 
