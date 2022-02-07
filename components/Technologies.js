3d5f72ecb3715792cf10aa8646f1c95cad820636
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-07 13:00:44 +0100

    Update Technologies.js

--- components/Technologies.js
+++ components/Technologies.js
@@ -6,33 +6,33 @@ function Technologies() {
                 <p className="text-center max-w-5xl mx-auto max-w-xl pt-5 pb-10 xxsm:text-md px-5 ssm:text-xl">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
             </div>
 
-            <div className='grid aboutlg:grid-cols-4 basemd:grid-cols-3 grid-cols-1 gap-4 text-center max-w-4xl mx-auto'>
+            <div className='grid aboutlg:grid-cols-4 basemd:grid-cols-3 grid-cols-2 gap-4 text-center max-w-4xl mx-auto'>
                 <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png'} alt="mockup" />
                     </div>
                     
 
 
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png'} alt="mockup" />
                     </div>
                     <div>
-                <img className="xxsm:h-28 xsm:h-36 mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png'} alt="mockup" />
+                <img className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto  " src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png'} alt="mockup" />
                     </div>
                     </div>
         </div>
