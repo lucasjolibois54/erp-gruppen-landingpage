8d97a07041f3653317dad4276f901465486fdad3
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-07 13:09:12 +0100

    Update Technologies.js

--- components/Technologies.js
+++ components/Technologies.js
@@ -9,7 +9,7 @@ function Technologies() {
           It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters, as opposed to using 'Content here, content here', making it
+          letters, as opposed to using Content here, content here, making it
           look like readable English.
         </p>
       </div>
