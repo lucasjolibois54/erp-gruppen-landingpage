7c48b9f96811abcebc8a3c73160a5f004589eb57
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-08 13:12:52 +0100

    Update Case.js

--- components/Case.js
+++ components/Case.js
@@ -7,12 +7,12 @@ function Case() {
       <div className="2xl:container 2xl:mx-auto">
         <div className="lg:px-20 md:px-6 px-4 md:py-12 py-8">
 
-          <div className="bg-card-bg-1 dark:bg-gray-800 p-8 mt-20">
+          <div className="bg-card-bg-1 p-8 mt-20">
             <div>
-              <h2 className="text-4xl leading-tight font-medium text-green-hl dark:text-white">
+              <h2 className="text-4xl leading-tight font-medium text-green-hl ">
                 Metal fencing
               </h2>
-              <p className="text-xl font-semibold text-green-hl dark:text-white mt-2">Hello</p>
+              <p className="text-xl font-semibold text-green-hl mt-2">Hello</p>
             </div>
             <div className="flex justify-center items-center mt-28 md:mt-1">
               <img
@@ -24,12 +24,12 @@ function Case() {
           </div>
 
           <div className="grid mb-10 grid-cols-1 lg:grid-cols-2 gap-5 md:gap-6 lg:gap-8 mt-4 md:mt-6 lg:mt-8">
-            <div className="bg-card-bg-2 dark:bg-gray-800 p-8">
+            <div className="bg-card-bg-2  p-8">
               <div>
-              <h2 className="text-4xl leading-tight font-medium text-green-hl dark:text-white">
+              <h2 className="text-4xl leading-tight font-medium text-green-hl ">
                 Gates and wickets
               </h2>
-              <p className="text-xl font-semibold text-green-hl dark:text-white mt-2">Hello</p>
+              <p className="text-xl font-semibold text-green-hl  mt-2">Hello</p>
               </div>
               <div className="flex justify-center items-center mt-28 md:mt-1">
                 <img
@@ -39,12 +39,12 @@ function Case() {
                 />
               </div>
             </div>
-            <div className="bg-card-bg-3 dar k:bg-gray-800 p-8">
+            <div className="bg-card-bg-3  p-8">
               <div>
-              <h2 className="text-4xl leading-tight font-medium text-green-hl dark:text-white">
+              <h2 className="text-4xl leading-tight font-medium text-green-hl ">
                 Fastenings and clamps.
               </h2>
-              <p className="text-xl font-semibold text-green-hl dark:text-white mt-2">Hello</p>
+              <p className="text-xl font-semibold text-green-hl  mt-2">Hello</p>
               </div>
               <div className="flex justify-center items-center mt-28 md:mt-1">
                 <img
