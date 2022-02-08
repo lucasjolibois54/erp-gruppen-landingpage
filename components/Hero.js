3f06d33e88d183c2a911930acde6acad33a07f5b
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-08 15:17:43 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,26 +1,23 @@
-
-
 function Hero() {
   return (
     <div>
-      <h1 className="text-4xl font-medium text-center leading-tight">Vi sikrer at virksomheder får <br /><span className="text-green-hl">professionel</span> hjælp til at få flyttet deres <br /> produktion op på en <span className="text-green-hl">moderne <br /> </span> fremtidssikret IT platform.</h1>
-      <div className="flex space-x-4 basemd:space-x-6 baselg:space-x-10 justify-center mt-8 mdsm:mt-16 basemd:mt-20">
-        <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} alt="mockup" />
-        <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto mt-6 lgbasemd:mt-10 base2lg:mt-20" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
-        <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} alt="mockup" />
+      <div className="dark:bg-gray-800 mx-80 my-auto mt-72 mb-96">
+        <div className="">
+          <h2 className="text-7xl leading-tight font-semibold dark:text-white">
+            Konsulenter i gulvhøjde <br />{" "}
+            <span className="text-green-hl">IT i skyen.</span>
+          </h2>
+          <p className="text-xl text-gray-400 dark:text-white mt-12">
+            ERP Produktion hjalp os med at strømline vores flow, <br />
+            og skabe en 20% tidsbesparelse i virksomheden.
+          </p>
+        </div>
+        <button className="bg-green-hl mt-8 text-white rounded-lg px-5 py-2 ">
+          Læs mere
+        </button>
       </div>
     </div>
-  )
-
+  );
 }
 
-export default Hero
-
-
-
-{/* <h1 className="text-lg xxsm:text-base xsm:text-xl basesm:text-2xl mdsm:text-3xl basemd:text-4xl baselg:text-5xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-center max-w-4xl mx-auto font-medium text-center mt-28">Vi sikrer at virksomheder får <span className="text-green-hl">professionel</span> hjælp til at få flyttet deres produktion op på en <span className="text-green-hl">moderne</span> fremtidssikret IT platform.</h1>
-<div className="flex space-x-4 basemd:space-x-6 baselg:space-x-10 justify-center mt-8 mdsm:mt-16 basemd:mt-20">
-  <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/austin-distel-wD1LRb9OeEo-unsplash_3_jnrrdn.png'} alt="mockup" />
-  <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto mt-6 lgbasemd:mt-10 base2lg:mt-20" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_26_wm8c3a.png'} alt="mockup" />
-  <img className="xxsm:h-28 xsm:h-36 mdsm:h-44 mdsm:h-44 basemd:h-60 baselg:h-96 w-auto" src={'https://res.cloudinary.com/ds8p0huok/image/upload/v1643884934/samples/image_27_po9vvq.png'} alt="mockup" />
-</div> */}
+export default Hero;
