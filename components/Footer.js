325aee7c594506062989842f54d915ff428e6cec
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-08 13:01:10 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,10 +4,10 @@ function Footer() {
     return (
       <div>
        
-          <div className="pt-20 pb-6">
+          <div className="pt-10 pb-6">
           <div className="border-b border-2 border-line mx-auto max-w-7xl w-5/6" />
               <div className="max-w-7xl mx-auto">
-        <p className=" text-lg xxsm:text-base xsm:text-xl basesm:text-xl mdsm:text-xl basemd:text-2xl baselg:text-2xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-xl font-medium mt-44 leading-loose">ERPgruppens Produktions facilitet udvikler og leverer brugerdefineret services, produkter. Support fra vores certificerede konsulenter.</p>
+        <p className=" text-lg xxsm:text-base xsm:text-xl basesm:text-xl mdsm:text-xl basemd:text-2xl baselg:text-2xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-xl font-medium mt-56 leading-loose">ERPgruppens Produktions facilitet udvikler og leverer brugerdefineret services, produkter. Support fra vores certificerede konsulenter.</p>
         </div>
         <p className="text-lg xxsm:text-base xsm:text-lg basesm:text-lg mdsm:text-lg basemd:text-xl baselg:text-4xl text-green-hl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-7xl mx-auto mt-32 underline text-red-400 font-semibold"><a  href="mailto:" >mail@erpgruppen.dk</a></p>
         <p className="text-lg xxsm:text-base xsm:text-lg basesm:text-lg mdsm:text-lg basemd:text-xl baselg:text-4xl text-green-hl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-7xl mx-auto mt-4"><a href="tel:004578794040" >+45 78 79 40 40</a></p>
