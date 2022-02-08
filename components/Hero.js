0c1613a2d386a1dba84ff55013813d2a1c47164c
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-08 15:34:51 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -1,13 +1,13 @@
 function Hero() {
   return (
     <div>
-      <div className="dark:bg-gray-800 mx-80 my-auto mt-72 mb-96">
+      <div className="dark:bg-gray-800 my-auto mx-auto mt-72 mb-96 max-w-7xl">
         <div className="">
-          <h2 className="text-7xl leading-tight font-semibold dark:text-white">
+          <h2 className="text-7xl leading-tight font-semibold">
             Konsulenter i gulvhøjde <br />{" "}
             <span className="text-green-hl">IT i skyen.</span>
           </h2>
-          <p className="text-xl text-gray-400 dark:text-white mt-12">
+          <p className="text-xl text-gray-400 mt-12">
             ERP Produktion hjalp os med at strømline vores flow, <br />
             og skabe en 20% tidsbesparelse i virksomheden.
           </p>
