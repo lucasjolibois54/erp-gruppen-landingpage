d3184d0c36df5a66a00be2ea98e2d3e87e8a01ac
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-08 09:26:39 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,7 @@
 function Footer() {
     return (
       <div>
+        <div className="border-b border-2 border-red-500 mx-auto w-3/6" />
           <div className="pt-20 pb-20">
               <div className="max-w-4xl mx-auto">
         <p className=" text-lg xxsm:text-base xsm:text-xl basesm:text-xl mdsm:text-xl basemd:text-2xl baselg:text-2xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-xl font-medium mt-16">ERPgruppens Produktions facilitet udvikler og leverer brugerdefineret services, produkter. Support fra vores certificerede konsulenter.</p>
