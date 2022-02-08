76fba1afdb6f4ceebd20c5bbcec27558e39721e6
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-02-08 15:14:16 +0100

    Update AboutHero.js

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -3,7 +3,7 @@ function AboutHero() {
         <div>
             <div>
                 <h1 className="text-center max-w-7xl font-semibold mx-auto pt-40 xxsm:text-3xl ssm:text-4xl mt-56">We’re built for software teams</h1>
-                <p className="max-w-5xl mx-auto max-w-3xl pt-10 xxsm:text-md ssm:text-xl p-10 mb-56">Our mission is to ensure teams can do their best work, no matter their size or budget. We focus on the details of everything we do.</p>
+                <p className="text-center max-w-5xl mx-auto max-w-3xl pt-10 xxsm:text-md ssm:text-xl p-10 mb-56">Our mission is to ensure teams can do their best work, no matter their size or budget. We focus on the details of everything we do.</p>
             </div>
         </div>
     )
