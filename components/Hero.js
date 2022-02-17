5c593fb509ac654c535e0aaa041dad4780a41db8
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-02-17 12:33:26 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ function Hero() {
       <div className=" my-auto mx-auto mt-56  mb-96 max-w-6xl">
         <div className="">
           <div className="flex">
-          <h2 className="text-8xl xxsm:text-6xl xxsm:ml-10 xxsm:max-w-sm sbasesm:max-w-sm sbasesm:mr-20 baselg:text-7xl baselg:mr-20 baselg:max-w-md base2lg:text-8xl base2lg:max-w-3xl font-bold">
+          <h2 className="text-8xl xxsm:text-4xl xxsm:ml-10 xxsm:max-w-xs sbasesm:max-w-sm sbasesm:mr-20 baselg:text-7xl baselg:mr-20 baselg:max-w-md base2lg:text-8xl base2lg:max-w-3xl font-bold">
             Konsulenter i gulvhøjde <br />{" "}
             <span className="text-green-hl">IT i skyen.</span>
           </h2>
@@ -16,13 +16,13 @@ function Hero() {
             alt="mockup"
           />
           </div>
-          <p className="text-xl text-gray-400 xxsm:ml-10 xxsm:text-sm xxsm:mt-8 xsm:mt-8 baselg:-mt-8">
-            ERP Produktion hjalp os med at strømline vores flow, <br />
+          <p className="text-xl text-gray-400 xxsm:ml-10 xxsm:pr-8 xxsm:text-sm xxsm:mt-8 xsm:mt-8 baselg:-mt-8">
+            ERP Produktion hjalp os med at strømline vores flow,
             og skabe en 20% tidsbesparelse i virksomheden.
           </p>
         </div>
-        <button className="bg-green-hl mt-8 xxsm:ml-10 text-white rounded-xl ourservice:px-10 ourservice:py-4 xxsm:px-4 xxsm:py-2 xsm:px-6 xsm:py-3">
-          Get a quote <span className="ml-6">➜</span>
+        <button className="bg-green-hl mt-8 xxsm:ml-4 text-white rounded-xl ourservice:px-10 ourservice:py-4 xxsm:px-4 xxsm:py-2 xxsm:px-4 xsm:px-6 xsm:py-3">
+          Get a quote <span className="ml-6 xxsm:ml-2">➜</span>
         </button>
         <button className="xsm:ml-10 xxsm:ml-3 underline">
           Learn more!
