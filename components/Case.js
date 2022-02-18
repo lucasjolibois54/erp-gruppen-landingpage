c897ef8928cd556e924de4be6559b26b24c3fab4
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-18 12:45:45 +0100

    Update Case.js

--- components/Case.js
+++ components/Case.js
@@ -1,11 +1,12 @@
 import { AnimateSharedLayout, motion } from "framer-motion";
 import { useState } from "react";
+import Link from 'next/link'
 
 const caseOne = [
   {
     id: 1,
     name: "Metal fencing",
-    href: "#",
+    href: "case",
     imageSrc:
       "https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg",
     imageAlt: "caseOne",
@@ -18,7 +19,7 @@ const caseTwo = [
   {
     id: 1,
     name: "Gates and wickets",
-    href: "#",
+    href: "case",
     imageSrc:
       "https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg",
     imageAlt: "caseTwo",
@@ -28,7 +29,7 @@ const caseTwo = [
   {
     id: 2,
     name: "Fastenings and clamps",
-    href: "#",
+    href: "case",
     imageSrc:
       "https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg",
     imageAlt: "caseTwo",
@@ -41,7 +42,7 @@ const caseThree = [
   {
     id: 1,
     name: "Additional security",
-    href: "#",
+    href: "case",
     imageSrc:
       "https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg",
     imageAlt: "caseThree",
@@ -57,13 +58,12 @@ function Case() {
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseOne.map((product) => (
+              <Link href={product.href}>
               <div key={product.id} className="group relative">
                 <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
-                    <a href={product.href}>
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                    </a>
                   </h3>
                   <img
                     src={product.imageSrc}
@@ -72,17 +72,19 @@ function Case() {
                   />
                 </div>
               </div>
+              </Link>
             ))}
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
             {caseTwo.map((product) => (
+              <Link href={product.href}>
               <div key={product.id} className="group relative">
                 <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
-                    <a href={product.href}>
+               
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                    </a>
+                
                   </h3>
                   <img
                     src={product.imageSrc}
@@ -91,17 +93,19 @@ function Case() {
                   />
                 </div>
               </div>
+              </Link>
             ))}
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseThree.map((product) => (
+              <Link href={product.href}>
               <div key={product.id} className="group relative">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
-                    <a href={product.href}>
+                  
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                    </a>
+               
                   </h3>
                   <img
                     src={product.imageSrc}
@@ -110,6 +114,7 @@ function Case() {
                   />
                 </div>
               </div>
+              </Link>
             ))}
           </div>
         </div>
