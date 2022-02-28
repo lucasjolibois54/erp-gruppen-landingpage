50e78b95a273289b7445831719f692baafc01234
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-28 12:29:44 +0100

    Update AboutHero.js

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -1,11 +1,11 @@
 function AboutHero() {
     return (
-        <div>
+        <div> 
             {/* <div>
                 <h1 className="text-center max-w-7xl font-semibold mx-auto pt-40 xxsm:text-3xl ssm:text-4xl mt-56">We’re built for software teams</h1>
                 <p className="text-center max-w-5xl mx-auto max-w-3xl pt-10 xxsm:text-md ssm:text-xl p-10 mb-56">Our mission is to ensure teams can do their best work, no matter their size or budget. We focus on the details of everything we do.</p>
             </div> */}
-
+ 
             <div>
                 <div className="max-w-7xl mx-auto px-10">
                     <h2 className="p-0 font-semibold text-black xxsm:text-xl xsm:text-2xl ourservice:text-3xl basemd:text-5xl baselg:text-6xl text-center base2lg:text-7xl big:text-7xl pt-0 md:pt-28 leading-regular">
