4cbcd1ccb200416652681ea28fb60867f6210071
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-02-28 14:16:47 +0100

    Create MoreAboutUs.js

---
+++ components/MoreAboutUs.js
@@ -0,0 +1,48 @@
+function MoreAboutUs() {
+    return (
+
+
+<div>
+    <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
+        <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
+        <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+            
+            <h2 className="text-4xl sm:text-5xl md:text-5xl 2xl:text-6xl font-black font-sans lg:mt-0 mt-4 text-dark-text py-0">
+            Learn more about us
+and our values
+               
+            </h2>
+            <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+            Development teams around the world, including NPR, Halfbrick, Duolingo, and Venmo use Firebase to ship their apps. Development teams around the world, including NPR, Halfbrick, Duolingo, and Venmo.
+ 
+            </p>
+            <div className="flex justify-center text-center items-center py-0 xl:justify-start">
+               
+                    <a href="#" class="  text-blue-500 underline decoration-1 font-bold py-2  transition duration-500 ease-in-out transform hover:scale-110">
+                    Learn more about us on our socials
+                    </a>
+                
+                
+
+              
+            </div>
+        </div>
+        <div className="w-full  xl:w-4/6">
+        <img
+                        className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto mt-20"
+                        src={
+                            "https://res.cloudinary.com/ds8p0huok/image/upload/v1646053577/image_44_2_bqnf0r.png"
+                        }
+                        alt="mockup"
+                    />
+
+        </div>
+    </div>
+
+    </div>
+    </div>
+    )
+
+}
+
+export default MoreAboutUs
