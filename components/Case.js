b97348b335509a23b524c837fff4a1de7dc83cae
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-02-28 10:34:48 +0100

    Update Case.js

--- components/Case.js
+++ components/Case.js
@@ -58,8 +58,8 @@ function Case() {
         <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseOne.map((product) => (
-              <Link href={product.href}>
-              <div key={product.id} className="group relative">
+              <Link key={product.id} href={product.href}>
+              <div className="group relative">
                 <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
                       <span aria-hidden="true" className="absolute inset-0" />
@@ -77,8 +77,8 @@ function Case() {
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
             {caseTwo.map((product) => (
-              <Link href={product.href}>
-              <div key={product.id} className="group relative">
+              <Link key={product.id} href={product.href}>
+              <div className="group relative">
                 <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
                
@@ -98,8 +98,8 @@ function Case() {
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseThree.map((product) => (
-              <Link href={product.href}>
-              <div key={product.id} className="group relative">
+              <Link key={product.id} href={product.href}>
+              <div className="group relative">
                 <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
                   <h3 className="text-5xl font-semibold p-5 absolute">
                   
