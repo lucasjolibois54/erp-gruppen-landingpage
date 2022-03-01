0ea7b895e8665c3edfa1e0b5ab965230dcfb75bd
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-01 10:34:12 +0100

    Update SocialMediaAbout.js

--- components/SocialMediaAbout.js
+++ components/SocialMediaAbout.js
@@ -4,10 +4,10 @@ function SocialMediaAbout() {
 
 <div>
     <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-        <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
+        <div id="top" className=" p-0 flex flex-col-reverse w-full xl:flex-row md:p-16 mb-4">
         <div className="w-full  xl:w-4/6">
         <img
-                        className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto mt-20"
+                        className=" pl-5 pr-5 xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto lg:mt-20"
                         src={
                             "https://res.cloudinary.com/ds8p0huok/image/upload/v1646124265/fbSocials_ywzxfd.png"
                         }
@@ -18,8 +18,8 @@ function SocialMediaAbout() {
         <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
             
             
-            <h2 className="text-4xl sm:text-5xl md:text-5xl 2xl:text-6xl font-black lg:mt-0 mt-4 text-dark-text py-0">
-            Find us on our socials
+            <h2 className="text-4xl sm:text-5xl md:text-5xl 2xl:text-6xl font-black lg:mt-0 mt-4 text-dark-text py-0 pt-28 lg:pt-20">
+            Find os på de sociale medier
                
             </h2>
             <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
@@ -29,7 +29,7 @@ function SocialMediaAbout() {
             <div className="flex justify-center text-center items-center py-0 xl:justify-start">
                
                     <a href="#" className="  text-blue-500 underline decoration-1 font-bold py-2  transition duration-500 ease-in-out transform hover:scale-110">
-                    Learn more about us on our socials
+                    Lær os bedre at gennem vores sociale medier
                     </a>
                 
                 
