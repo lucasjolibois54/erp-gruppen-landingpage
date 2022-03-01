e33eb424ebb22b4e8fa5a079e73cf36e892952be
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-03-01 12:36:42 +0100

    Update MoreAboutUs.js

--- components/MoreAboutUs.js
+++ components/MoreAboutUs.js
@@ -4,20 +4,20 @@ function MoreAboutUs() {
 
 <div>
     <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-        <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
-        <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+        <div id="top" className=" p-0 flex flex-col w-full lg:flex-row md:p-16 mb-4">
+        <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left lg:pl-10">
             
             <h2 className="text-4xl sm:text-5xl md:text-5xl 2xl:text-6xl font-bold lg:mt-0 pt-20 text-dark-text py-0">
             Lær os bedre at kende
                
             </h2>
-            <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+            <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 lg:pl-0 lg:pr-20">
             ERP produktion er et IT- og konsulenthus, der leverer support, rådgivning, IT-løsninger og kurser inden for Microsoft Dynamics og Microsoft Power Platform. Vi har mere end 15 års erfaring i branchen med at levere professionelle forretningsløsninger til små og mellemstore virksomheder i Danmark.
 
 
  
             </p>
-            <div className="flex justify-center text-center items-center py-0 xl:justify-start">
+            <div className="flex justify-center text-center items-center py-0 lg:justify-start">
                
                     <a href="#" className="  text-blue-500 underline decoration-1 font-bold py-2  transition duration-500 ease-in-out transform hover:scale-110">
                     Læs mere om hvem vi er
