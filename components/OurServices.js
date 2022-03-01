eb0765835cb5d0d331b3e209750b8628322b44db
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-03-01 10:01:56 +0100

    Update OurServices.js

--- components/OurServices.js
+++ components/OurServices.js
@@ -7,7 +7,7 @@ function OurServices() {
         </h1>
       </div>
 
-      <div className="grid aboutlg:grid-cols-4 xsbasesm:grid-cols-1 xxsm:p-4 smdsm:grid-cols-2 grid-cols-1 xxsm:gap-6 max-w-7xl mx-auto mb-20 mt-10">
+      <div className="grid aboutlg:grid-cols-4 xsbasesm:grid-cols-1 xxsm:p-4 smdsm:grid-cols-2 baselg:grid-cols-3 base2lg:grid-cols-4 grid-cols-1 xxsm:gap-6 max-w-7xl mx-auto mb-20 mt-10">
         <div className="p-2 shadow rounded">
           <h2 className="text-center font-semibold basemd:text-4xl xxsm:text-3xl mt-16">Functional Consultant</h2>
           <p className="p-10">
@@ -58,6 +58,16 @@ function OurServices() {
           </p>
         </div>
       </div>
+        <div className="text-center pb-6 -mt-10">
+                <a href="#about" >
+                    <button href="#" className="shadow-lg bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110">
+                    view cases
+                    </button>
+                </a>
+                
+
+              
+            </div>
     </div>
     )
 
