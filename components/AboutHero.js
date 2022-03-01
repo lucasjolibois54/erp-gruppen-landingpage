53da9b5efa0a1540be9704ccdebd93095dca316a
Lucas Bendix Jolibois <74597842+lucasjolibois54@users.noreply.github.com>
2022-03-01 12:36:39 +0100

    Update AboutHero.js

--- components/AboutHero.js
+++ components/AboutHero.js
@@ -4,18 +4,18 @@ function AboutHero() {
 
 <div>
     <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-        <div id="top" className=" p-0 flex flex-col w-full xl:flex-row md:p-0 mb-4">
-        <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
+        <div id="top" className=" p-0 flex flex-col w-full lg:flex-row md:p-0 mb-4">
+        <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left lg:pl-10">
             
             <h2 className="text-4xl sm:text-5xl md:text-5xl 2xl:text-6xl font-bold lg:mt-0 mt-4 text-dark-text py-0">
             Team af eksperter i  
                 <span className="text-blue-500"> marketing & produktion</span>
             </h2>
-            <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
+            <p className="text-base md:text-xl px-5 text-dark-text py-5 md:px-24 lg:pl-0 lg:pr-20">
             Development teams around the world, including NPR, Halfbrick, Duolingo, and Venmo use Firebase to ship their apps. Development teams around the world, including NPR, Halfbrick, Duolingo, and Venmo.
  
             </p>
-            <div className="flex justify-center text-center items-center py-0 xl:justify-start">
+            <div className="flex justify-center text-center items-center py-0 lg:justify-start">
                 <a href="#about" >
                     <button href="#" className="shadow-lg bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full transition duration-500 ease-in-out transform hover:scale-110">
                     Request a demo
