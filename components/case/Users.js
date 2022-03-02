31a1e68b17fbfe561616b3e78dd39315280ad321
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-03-02 16:53:40 +0100

    Create Users.js

---
+++ components/case/Users.js
@@ -0,0 +1,70 @@
+export const Users = [
+  {
+    name: "Deepak",
+    rollNo: "123",
+  },
+  {
+    name: "Yash",
+    rollNo: "124",
+  },
+  {
+    name: "Raj",
+    rollNo: "125",
+  },
+  {
+    name: "Rohan",
+    rollNo: "126",
+  },
+  {
+    name: "Puneet",
+    rollNo: "127",
+  },
+  {
+    name: "Vivek",
+    rollNo: "128",
+  },
+  {
+    name: "Aman",
+    rollNo: "129",
+  },
+];
+
+export const Cabola = [
+  {
+    id: 1,
+    name: "Cabola ApS",
+    title: "Dynamics 365 Business Central",
+    textBox1:
+      "Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.",
+    textBox2:
+      "Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.",
+    textBox3:
+      "Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.",
+    href: "case",
+    imageSrc:
+      "https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
+    imageAlt: "caseOne",
+    price: "caseOne",
+    color: "caseOne",
+  },
+];
+
+export const Apple = [
+  {
+    id: 2,
+    name: "Apple",
+    title: "Appstore Center 365",
+    textBox1:
+      "Apple1 Apple1 Apple1 Apple1",
+    textBox2:
+      "Apple2 Apple2 Apple2 Apple2",
+    textBox3:
+      "Apple3 Apple3 Apple3 Apple3",
+    href: "case",
+    imageSrc:
+      "https://www.apple.com/v/home/aj/images/promos/airpods/tile_airpods__fuv6vgn42y2q_small.jpg",
+    imageAlt: "caseOne",
+    price: "caseOne",
+    color: "caseOne",
+  },
+];
