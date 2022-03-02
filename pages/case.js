48bc748d8de34d53d513bb7353d015dc4ee64988
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-03-02 17:02:07 +0100

    Update case.js

--- pages/case.js
+++ pages/case.js
@@ -8,27 +8,6 @@ import Info from "../components/case/Info";
 //components
 import Navigation from "../components/Navigation";
 import Header from "../components/Header";
-import Modal from "../components/modals/Modal";
-
-const caseOne = [
-  {
-    id: 1,
-    name: "Cabola ApS",
-    title: "Dynamics 365 Business Central",
-    textBox1:
-      "Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.",
-    textBox2:
-      "Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.",
-    textBox3:
-      "Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.",
-    href: "case",
-    imageSrc:
-      "https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8",
-    imageAlt: "caseOne",
-    price: "caseOne",
-    color: "caseOne",
-  },
-];
 
 function Case() {
   const [toggleMenu, setToggleMenu] = useState(false);
