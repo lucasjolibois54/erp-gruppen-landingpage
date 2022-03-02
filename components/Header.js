03d0c018839026eb694c3494bc130e64e4a9a86a
Andreas E. Eriksen <AndreasEriksen99@hotmail.com>
2022-03-02 17:51:48 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,9 +2,11 @@ import Link from "next/link";
 import React, { useEffect, useRef } from "react"
 // Styled Components
 import { Container, Flex } from "../styles/globalStyles"
-import { HeaderNav, Logo, Menu } from "../styles/headerStyles"
 import Image from 'next/image'
 import ERPLogo from '/public/erp.svg'
+
+import styled from "styled-components"
+import { motion } from "framer-motion"
 
 const Header = ({
   setToggleMenu,
@@ -43,3 +45,50 @@ const Header = ({
 }
 
 export default Header
+
+export const HeaderNav = styled(motion.div)`
+  height: 0px;
+  width: 100%;
+  position: relative;
+  top: 62px;
+  right: 0;
+  left: 0;
+  z-index: 101;
+  margin-bottom: 150px;
+`
+export const Logo = styled.div`
+  a {
+    font-size: 1.8rem;
+    text-decoration: none;
+    font-weight: 800;
+    color: #000;
+    color: ${props => props.theme.text};
+  }
+  span {
+    height: 16px;
+    width: 16px;
+    background: #ea291e;
+    margin: 0 4px;
+    display: inline-block;
+    position: relative;
+    bottom: 2px;
+    cursor: pointer;
+  }
+`
+
+export const Menu = styled.div`
+  button {
+    transform-origin: center;
+    border: none;
+    padding: 20px;
+    background: none;
+    outline: none;
+    span {
+      width: 36px;
+      height: 5px;
+      display: block;
+      background: black;
+      margin: 8px;
+    }
+  }
+`
