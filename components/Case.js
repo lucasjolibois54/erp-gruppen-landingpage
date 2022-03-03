09ecb860503d725b3aa77442b6aedc74f9f4360f
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-03 17:15:56 +0100

    Update Case.js

--- components/Case.js
+++ components/Case.js
@@ -1,55 +1,55 @@
-import { AnimateSharedLayout, motion } from "framer-motion";
-import { useState } from "react";
+import { AnimateSharedLayout, motion } from 'framer-motion'
+import { useState } from 'react'
 import Link from 'next/link'
 
 const caseOne = [
   {
     id: 1,
-    name: "Metal fencing",
-    href: "case",
+    name: 'Metal fencing',
+    href: 'case/cabola',
     imageSrc:
-      "https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg",
-    imageAlt: "caseOne",
-    price: "caseOne",
-    color: "caseOne",
+      'https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg',
+    imageAlt: 'caseOne',
+    price: 'caseOne',
+    color: 'caseOne',
   },
-];
+]
 
 const caseTwo = [
   {
     id: 1,
-    name: "Gates and wickets",
-    href: "case",
+    name: 'Gates and wickets',
+    href: 'case/apple',
     imageSrc:
-      "https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg",
-    imageAlt: "caseTwo",
-    price: "caseTwo ",
-    color: "caseTwo ",
+      'https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg',
+    imageAlt: 'caseTwo',
+    price: 'caseTwo ',
+    color: 'caseTwo ',
   },
   {
     id: 2,
-    name: "Fastenings and clamps",
-    href: "case",
+    name: 'Fastenings and clamps',
+    href: 'case/apple',
     imageSrc:
-      "https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg",
-    imageAlt: "caseTwo",
-    price: "caseTwo ",
-    color: "caseTwo ",
+      'https://www.apple.com/v/home/aj/images/heroes/watch-series-7/hero_watch__f05go333pz2i_small_2x.jpg',
+    imageAlt: 'caseTwo',
+    price: 'caseTwo ',
+    color: 'caseTwo ',
   },
-];
+]
 
 const caseThree = [
   {
     id: 1,
-    name: "Additional security",
-    href: "case",
+    name: 'Additional security',
+    href: 'case/apple',
     imageSrc:
-      "https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg",
-    imageAlt: "caseThree",
-    price: "caseThree",
-    color: "caseThree",
+      'https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg',
+    imageAlt: 'caseThree',
+    price: 'caseThree',
+    color: 'caseThree',
   },
-];
+]
 
 function Case() {
   return (
@@ -59,67 +59,63 @@ function Case() {
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseOne.map((product) => (
               <Link key={product.id} href={product.href}>
-              <div className="group relative">
-                <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                  <h3 className="text-5xl font-semibold p-5 absolute">
+                <div className="group relative">
+                  <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
+                    <h3 className="text-5xl font-semibold p-5 absolute">
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                  </h3>
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
+                    </h3>
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
                 </div>
-              </div>
               </Link>
             ))}
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
             {caseTwo.map((product) => (
               <Link key={product.id} href={product.href}>
-              <div className="group relative">
-                <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                  <h3 className="text-5xl font-semibold p-5 absolute">
-               
+                <div className="group relative">
+                  <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
+                    <h3 className="text-5xl font-semibold p-5 absolute">
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-                
-                  </h3>
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
+                    </h3>
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
                 </div>
-              </div>
               </Link>
             ))}
           </div>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
             {caseThree.map((product) => (
               <Link key={product.id} href={product.href}>
-              <div className="group relative">
-                <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                  <h3 className="text-5xl font-semibold p-5 absolute">
-                  
+                <div className="group relative">
+                  <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
+                    <h3 className="text-5xl font-semibold p-5 absolute">
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
-               
-                  </h3>
-                  <img
-                    src={product.imageSrc}
-                    alt={product.imageAlt}
-                    className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                  />
+                    </h3>
+                    <img
+                      src={product.imageSrc}
+                      alt={product.imageAlt}
+                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                    />
+                  </div>
                 </div>
-              </div>
               </Link>
             ))}
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
-export default Case;
+export default Case
