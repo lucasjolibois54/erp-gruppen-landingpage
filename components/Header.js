507142cb46b8e3ca1f232c17e306eb1ef37b2a6b
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-03 12:22:31 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,10 @@
 import Link from 'next/link'
 import React, { useEffect, useRef } from 'react'
 // Styled Components
-import { Container, Flex } from '../styles/globalStyles'
 import Image from 'next/image'
 import ERPLogo from '/public/erp.svg'
 
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { motion } from 'framer-motion'
 
 const Header = ({ setToggleMenu, toggleMenu }) => {
@@ -19,70 +18,23 @@ const Header = ({ setToggleMenu, toggleMenu }) => {
         ease: [0.6, 0.05, -0.01, 0.9],
       }}
     >
-      <Container>
-        <Flex spaceBetween noHeight>
-          <Logo>
+      <motion.div className="grow m-auto px-5 relative max-w-7xl h-full">
+        <motion.div className="relative flex items-center justify-between h-0">
+          <div className="text-lg no-underline font-bold text-black ">
             <Link href="/">
               <Image width={250} src={ERPLogo} />
             </Link>
-          </Logo>
-          <Menu onClick={() => setToggleMenu(!toggleMenu)}>
-            <button>
-              <span></span>
-              <span></span>
+          </div>
+          <motion.div onClick={() => setToggleMenu(!toggleMenu)}>
+            <button className="origin-center b-none p-20 outline-none">
+              <span className="w-9 h-1 block bg-black m-2" />
+              <span className="w-9 h-1 block bg-black m-2" />
             </button>
-          </Menu>
-        </Flex>
-      </Container>
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </motion.div>
   )
 }
 
 export default Header
-
-export const HeaderNav = styled(motion.div)`
-  height: 0px;
-  width: 100%;
-  position: relative;
-  top: 62px;
-  right: 0;
-  left: 0;
-  z-index: 101;
-  margin-bottom: 150px;
-`
-export const Logo = styled.div`
-  a {
-    font-size: 1.8rem;
-    text-decoration: none;
-    font-weight: 800;
-    color: #000;
-    color: ${(props) => props.theme.text};
-  }
-  span {
-    height: 16px;
-    width: 16px;
-    background: #ea291e;
-    margin: 0 4px;
-    display: inline-block;
-    position: relative;
-    bottom: 2px;
-    cursor: pointer;
-  }
-`
-
-export const Menu = styled.div`
-  button {
-    transform-origin: center;
-    border: none;
-    padding: 20px;
-    background: none;
-    outline: none;
-    span {
-      width: 36px;
-      height: 5px;
-      display: block;
-      background: black;
-      margin: 8px;
-    }
-  }
-`
