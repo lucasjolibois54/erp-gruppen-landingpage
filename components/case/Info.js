ff697554368975a3a46029930763c4113b14a01a
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-03 17:15:58 +0100

    Update Info.js

--- components/case/Info.js
+++ components/case/Info.js
@@ -1,17 +1,21 @@
-import React from 'react';
- 
-function Info (props){
+import React from 'react'
+//Components
+import { OpenOutline } from 'react-ionicons'
+import Modal from '../modals/Modal'
+
+function Info(props) {
   return (
     <div key={props.id} className="grid max-w-7xl mx-auto">
       <div className="group relative mt-20">
         <div className="md:flex">
           <div className="w-full min-h-80 aspect-w-1 p-5 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none">
-            <h3 className="text-5xl flex font-semibold">
-              {props.name}
-            </h3>
+            <div className="flex">
+              <h3 className="text-5xl flex font-semibold">{props.name}</h3>
+              <Modal />
+            </div>
             <span
-              aria-hidden="true"
               className="text-gray-300 w-96 mt-5 text-4xl font-regular flex"
+              aria-hidden="true"
             >
               {props.title}
             </span>
@@ -26,26 +30,31 @@ function Info (props){
           alt={props.imageAlt}
         />
         <div className="w-full aspect-w-1 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none">
-          <div className="flex mx-auto justify-center">
-            <h3 className="m-12 text-md">
+          <div className="mx-auto justify-center lg:grid-cols-2 grid">
+            <h3 className="m-8 font-bold text-md bg-blue-50 rounded-xl p-8">
               Strategy
-              <span className="flex text-sm m-3">Hello</span>
-              <span className="flex text-sm m-3">Hello</span>
+              <span className="flex font-medium text-sm mt-3 leading-tight">
+                Cabolas forretningsstrategi blev fremtidssikret med op til 17x
+                mere sandsynlighed.
+              </span>
             </h3>
-            <h3 className="m-12 text-md">
+            <h3 className="m-8 font-bold text-md bg-red-50 rounded-xl p-8">
               User Experience
-              <span className="flex text-sm m-3">Hello</span>
-              <span className="flex text-sm m-3">Hello</span>
+              <span className="flex font-medium text-sm mt-3 leading-tight">
+                Hello
+              </span>
             </h3>
-            <h3 className="m-12 text-md">
-              propsion
-              <span className="flex text-sm m-3">Hello</span>
-              <span className="flex text-sm m-3">Hello</span>
+            <h3 className="m-8 font-bold text-md bg-green-50 rounded-xl p-8">
+              Production
+              <span className="flex font-medium text-sm mt-3 leading-tight">
+                Hello
+              </span>
             </h3>
-            <h3 className="m-12 text-md">
+            <h3 className="m-8 font-bold text-md bg-purple-50 rounded-xl p-8">
               Platforms
-              <span className="flex text-sm m-3">Hello</span>
-              <span className="flex text-sm m-3">Hello</span>
+              <span className="flex font-medium text-sm mt-3 leading-tight">
+                Hello
+              </span>
             </h3>
           </div>
           <div className="mt-20 mx-12">
@@ -57,30 +66,9 @@ function Info (props){
               <span className="mt-5 flex">{props.textBox3}</span>
             </h3>
           </div>
-          <div className="mt-20 1xl:mt-60 justify-center mx-12">
-            <div className="flex invisible h-0 lgbasemd:h-auto lgbasemd:visible">
-              <img
-                className="h-16 w-auto pl-6"
-                src={
-                  "https://res.cloudinary.com/ds8p0huok/image/upload/v1645175524/Ellipse_92_wpnmfl.png"
-                }
-                alt="mockup"
-              />
-              <div className="flex flex-col items-center justify-center pl-2">
-                <h2 className="font-bold">Niels pedersen</h2>
-                <p>CEO, Cabola ApS</p>
-              </div>
-            </div>
-            <div className="p-10 w-2/3 items-center justify-center invisible h-0 lgbasemd:h-auto lgbasemd:visible lgbasemd:space-x-6 smbaselg:space-x-16 aboutlg:space-x-20 baselg:space-x-28 base2lg:space-x-36">
-              <blockquote className="pt-1 pl-1">
-                &gt;{props.textBox3}
-              </blockquote>
-            </div>
-          </div>
         </div>
       </div>
-  </div>
-
+    </div>
   )
 }
-export default Info;
+export default Info
