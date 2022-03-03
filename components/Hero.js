9ffa2592bf385918ae6d3d2015b0571101bde6a4
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-03 16:08:19 +0100

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
       <div className="">
         <video autoPlay muted loop className="absolute smbig:max-h-screen smbig:w-full top-0 -z-50 xxsm:invisible basemd:h-5/6 lgbasemd:h-auto basemd:visible w-auto">
           <source
-            src="https://res.cloudinary.com/dckwf6med/video/upload/v1646318905/erp/Untitled_xo3dxl.mp4"
+            src="https://res.cloudinary.com/dckwf6med/video/upload/v1646318247/erp/C0047_zrfbbe.mp4"
             type="video/mp4"
           />
         </video>
