847f22fdc51e1e9337b088437f2b1f44f6aefc65
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-03 17:16:33 +0100

    Update Modal.js

--- components/modals/Modal.js
+++ components/modals/Modal.js
@@ -1,20 +1,22 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useRef, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { ExclamationIcon } from "@heroicons/react/outline";
-import { OpenOutline } from "react-ionicons";
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import {
+  ExclamationIcon,
+  QuestionMarkCircleIcon,
+} from '@heroicons/react/outline'
+import { OpenOutline } from 'react-ionicons'
 
 export default function Modal() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false)
 
-  const cancelButtonRef = useRef(null);
+  const cancelButtonRef = useRef(null)
 
   return (
     <div>
-      {/* <!-- Modal toggle --> */}
-      <button onClick={() => setOpen(true)} type="button">
-        <OpenOutline color={"#3B82F6"} title="" height="45px" width="45px" />
-      </button>
+      <div onClick={() => setOpen(true)} className="w-12 h-12 text-blue-500">
+        <OpenOutline color={'#3B82F6'} title="" height="45px" width="45px" />
+      </div>
       <Transition.Root show={open} as={Fragment}>
         <Dialog
           as="div"
@@ -54,9 +56,9 @@ export default function Modal() {
               <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
                 <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationIcon
-                        className="h-6 w-6 text-red-600"
+                    <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-blue-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <QuestionMarkCircleIcon
+                        className="h-6 w-6 text-blue-500"
                         aria-hidden="true"
                       />
                     </div>
@@ -69,7 +71,8 @@ export default function Modal() {
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Er du sikker på at du gerne vil besøge https://www.cabola.dk
+                          Er du sikker på at du gerne vil besøge
+                          https://www.cabola.dk
                         </p>
                       </div>
                     </div>
@@ -78,14 +81,16 @@ export default function Modal() {
                 <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
-                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                    onClick={event => window.location.href="https://www.cabola.dk"}
+                    className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white sm:ml-3 sm:w-auto sm:text-sm"
+                    onClick={(event) =>
+                      (window.location.href = 'https://www.cabola.dk')
+                    }
                   >
                     Ja
                   </button>
                   <button
                     type="button"
-                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
                     onClick={() => setOpen(false)}
                     ref={cancelButtonRef}
                   >
@@ -98,5 +103,5 @@ export default function Modal() {
         </Dialog>
       </Transition.Root>
     </div>
-  );
+  )
 }
