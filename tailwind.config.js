2c900773dd3a74a0e39ac19b26a895be8ec6bad4
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-03 11:41:20 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
+      zIndex: {
+        '150': '150',
+      },
       colors: {
         //background colors
         'green-hl': '#204B45',
@@ -15,78 +18,75 @@ module.exports = {
         'card-bg-4': '#F1F1FD',
         'card-bg-5': '#FDF3F1',
         // Line Color
-        'line': '#F6F6F6',
+        line: '#F6F6F6',
         // text
         'text-gray': '#8A8A8A',
-
       },
       screens: {
-        'xxsm': '220px',
-        // => @media (min-width: 520px) { ... }
-      
-        'xsm': '340px',
+        xxsm: '220px',
         // => @media (min-width: 520px) { ... }
 
-        'ourservice': '420px',
+        xsm: '340px',
+        // => @media (min-width: 520px) { ... }
+
+        ourservice: '420px',
 
         'abouthero-screen': '400px',
-        'abouthero2screen': '370px',
+        abouthero2screen: '370px',
 
-        'ssm': '430px',
+        ssm: '430px',
         // => @media (min-width: 520px) { ... }
 
-        'basesm': '450px',
+        basesm: '450px',
         // => @media (min-width: 520px) { ... }
 
-        'xsbasesm': '550px',
+        xsbasesm: '550px',
         // => @media (min-width: 520px) { ... }
 
-        'smdsm': '600px',
+        smdsm: '600px',
         // => @media (min-width: 440px) { ... }
 
-        'mdsm': '630px',
+        mdsm: '630px',
         // => @media (min-width: 440px) { ... }
 
-        'sbasesm': '670px',
+        sbasesm: '670px',
         // => @media (min-width: 440px) { ... }
-      
-        'basemd': '740px',
-        // => @media (min-width: 768px) { ... }
-      
-        'lgbasemd': '770px',
+
+        basemd: '740px',
         // => @media (min-width: 768px) { ... }
 
-        
-        'smbaselg': '900px',
+        lgbasemd: '770px',
+        // => @media (min-width: 768px) { ... }
+
+        smbaselg: '900px',
         // => @media (min-width: 1024px) { ... }
 
-        'aboutlg': '1000px',
+        aboutlg: '1000px',
         // => @media (min-width: 1024px) { ... }
 
-        'baselg': '1024px',
+        baselg: '1024px',
         // => @media (min-width: 1024px) { ... }
-      
-        'base2lg': '1148px',
+
+        base2lg: '1148px',
         // => @media (min-width: 1148px) { ... }
-      
-        'xl': '1280px',
+
+        xl: '1280px',
         // => @media (min-width: 1280px) { ... }
 
         '1xl': '1342px',
         // => @media (min-width: 1280px) { ... }
-      
+
         '2xl': '1536px',
 
-        'smbig': '1920px',
+        smbig: '1920px',
         // => @media (min-width: 1536px) { ... }
-        'big': '2550px',
-      },},
-  },
- 
-    variants: {
-      extend: {},
+        big: '2550px',
+      },
     },
-    plugins: [
-      
-    ],
-  }
+  },
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
