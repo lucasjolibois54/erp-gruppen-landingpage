c5c42d0084d6ece60f1c4a0bfef882224372dc64
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-04 16:11:09 +0100

    Update CaseThumbnail.js

--- components/CaseThumbnail.js
+++ components/CaseThumbnail.js
@@ -2,10 +2,10 @@ import { AnimateSharedLayout, motion } from 'framer-motion'
 import { useState } from 'react'
 import Link from 'next/link'
 
-const caseOne = [
+const cases = [
   {
     id: 1,
-    name: 'Metal fencing',
+    name: 'Cabola ApS',
     href: 'case/cabola',
     imageSrc:
       'https://www.apple.com/v/home/aj/images/heroes/iphone-13-pro/hero_iphone_13pro__dhnsegysa42u_small_2x.jpg',
@@ -13,11 +13,8 @@ const caseOne = [
     price: 'caseOne',
     color: 'caseOne',
   },
-]
-
-const caseTwo = [
   {
-    id: 1,
+    id: 2,
     name: 'Gates and wickets',
     href: 'case/apple',
     imageSrc:
@@ -27,7 +24,7 @@ const caseTwo = [
     color: 'caseTwo ',
   },
   {
-    id: 2,
+    id: 3,
     name: 'Fastenings and clamps',
     href: 'case/apple',
     imageSrc:
@@ -36,11 +33,8 @@ const caseTwo = [
     price: 'caseTwo ',
     color: 'caseTwo ',
   },
-]
-
-const caseThree = [
   {
-    id: 1,
+    id: 4,
     name: 'Additional security',
     href: 'case/apple',
     imageSrc:
@@ -51,68 +45,61 @@ const caseThree = [
   },
 ]
 
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1.8,
+    transition: {
+      delayChildren: 0.8,
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const item = {
+  hidden: { y: 0, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
 function Case() {
   return (
     <div>
       <div className="">
-        <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
-            {caseOne.map((product) => (
-              <Link key={product.id} href={product.href}>
-                <div className="group relative">
-                  <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                    <h3 className="text-5xl font-semibold p-5 absolute">
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </h3>
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                    />
+        <div className="w-full h-screen overflow-hidden p-0 m-0 flex justify-center items-center">
+          <motion.ul
+            className="w-1/2 h-1/2 grid overflow-hidden m-0 list-none grid-cols-2 grid-rows-1/2 gap-4 p-4 bg-blue-500 rounded-lg"
+            variants={container}
+            initial="hidden"
+            animate="visible"
+          >
+            {cases.map((props) => (
+              <motion.li
+                key={props.id}
+                className=" rounded-xl overflow-hidden"
+                variants={item}
+              >
+                <Link key={props.id} href={props.href}>
+                  <div className="group relative">
+                    <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none">
+                      <h3 className="text-5xl font-semibold p-5 absolute">
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {props.name}
+                      </h3>
+                      <img
+                        src={props.imageSrc}
+                        alt={props.imageAlt}
+                        className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+                      />
+                    </div>
                   </div>
-                </div>
-              </Link>
+                </Link>
+              </motion.li>
             ))}
-          </div>
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-2 xl:gap-x-8">
-            {caseTwo.map((product) => (
-              <Link key={product.id} href={product.href}>
-                <div className="group relative">
-                  <div className="w-full bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                    <h3 className="text-5xl font-semibold p-5 absolute">
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </h3>
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                    />
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
-          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-1 lg:grid-cols-1 xl:gap-x-8">
-            {caseThree.map((product) => (
-              <Link key={product.id} href={product.href}>
-                <div className="group relative">
-                  <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 overflow-hidden group-hover:opacity-75 lg:h-full lg:aspect-none">
-                    <h3 className="text-5xl font-semibold p-5 absolute">
-                      <span aria-hidden="true" className="absolute inset-0" />
-                      {product.name}
-                    </h3>
-                    <img
-                      src={product.imageSrc}
-                      alt={product.imageAlt}
-                      className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                    />
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+          </motion.ul>
         </div>
       </div>
     </div>
