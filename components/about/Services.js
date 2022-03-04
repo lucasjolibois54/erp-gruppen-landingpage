4f84dd955fc54481bae69937568c7c9d6b394de7
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-03-04 12:50:39 +0100

    Create Services.js

---
+++ components/about/Services.js
@@ -0,0 +1,67 @@
+function OurServices() {
+    return (
+        <div className="bg-black text-white pb-5">
+      <div>
+        <h1 className="text-center max-w-7xl font-semibold mx-auto pt-20 xxsm:text-3xl basemd:text-5xl ssm:text-4xl">
+          Vores tjenester
+        </h1>
+      </div>
+
+      <div className="grid aboutlg:grid-cols-4 xsbasesm:grid-cols-1 xxsm:p-4 smdsm:grid-cols-2 baselg:grid-cols-3 base2lg:grid-cols-4 grid-cols-1 xxsm:gap-6 max-w-7xl mx-auto mb-20 mt-10">
+        <div className="p-2 shadow rounded">
+          <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Functional Consultant</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Produktion</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Developer</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">SupplyChain</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Azure</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Business Central</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">TimeXtender</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+        <div className="p-2 shadow rounded">
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Power BI</h2>
+          <p className="mt-6">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          </p>
+        </div>
+      </div>
+        
+    </div>
+    )
+
+}
+
+export default OurServices
