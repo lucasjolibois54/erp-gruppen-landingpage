a10527c06c47d07ff94c9f5d2e0d3e49dd21f937
Daudmir <74597831+Daudmir@users.noreply.github.com>
2022-03-04 14:43:13 +0100

    Update Services.js

--- components/about/Services.js
+++ components/about/Services.js
@@ -1,60 +1,64 @@
+import Fade from 'react-reveal/Fade'
+
+
 function OurServices() {
     return (
+      
         <div className="bg-black text-white pb-5">
       <div>
         <h1 className="text-center max-w-7xl font-semibold mx-auto pt-20 xxsm:text-3xl basemd:text-5xl ssm:text-4xl">
-          Vores tjenester
+          <Fade>Vores tjenester</Fade>
         </h1>
       </div>
 
       <div className="grid aboutlg:grid-cols-4 xsbasesm:grid-cols-1 xxsm:p-4 smdsm:grid-cols-2 baselg:grid-cols-3 base2lg:grid-cols-4 grid-cols-1 xxsm:gap-6 max-w-7xl mx-auto mb-20 mt-10">
         <div className="p-2 shadow rounded">
-          <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Functional Consultant</h2>
+          <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Functional Consultant</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Produktion</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Produktion</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Developer</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Developer</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">SupplyChain</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>SupplyChain</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
 
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Azure</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Azure</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Business Central</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Business Central</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">TimeXtender</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>TimeXtender</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
         <div className="p-2 shadow rounded">
-        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16">Power BI</h2>
+        <h2 className="font-semibold basemd:text-2xl xxsm:text-3xl mt-16"><Fade>Power BI</Fade></h2>
           <p className="mt-6">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          <Fade>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Fade>
           </p>
         </div>
       </div>
