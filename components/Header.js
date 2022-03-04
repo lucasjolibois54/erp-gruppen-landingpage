75413a2bab9df6cc6f3d05559e4323ab476811ac
AeeUX <AndreasEriksen99@hotmail.com>
2022-03-04 12:39:50 +0100

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -25,10 +25,7 @@ const Header = ({ setToggleMenu, toggleMenu }) => {
               <Image width={250} src={ERPLogo} />
             </Link>
           </div>
-          <motion.div
-            className="mix-blend-exclusion"
-            onClick={() => setToggleMenu(!toggleMenu)}
-          >
+          <motion.div className="" onClick={() => setToggleMenu(!toggleMenu)}>
             <button className="origin-center b-none p-5 outline-none">
               <span className="w-9 h-1 block bg-black m-2" />
               <span className="w-9 h-1 block bg-black m-2" />
