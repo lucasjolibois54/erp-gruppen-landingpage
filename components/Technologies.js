5ac9d86b0f75e15eb34f8707bf95c228f4263ea5
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-07 15:22:36 +0100

    Update Technologies.js

--- components/Technologies.js
+++ components/Technologies.js
@@ -2,96 +2,96 @@ function Technologies() {
   return (
     <div>
       <div className="mb-28 lg:mt-20">
-      <div>
-        <h1 className="text-center max-w-6xl font-semibold mx-auto pt-40 xxsm:text-xl basemd:text-5xl ssm:text-4xl">
-          Certificeret og erfarende konsulenter
-        </h1>
-        <p className="text-center max-w-6xl mx-auto max-w-xl pt-8 pb-10 xxsm:text-md px-5 ssm:text-xl ssm:leading-relaxed">
-        Vi er et velerfarende og certificeret team med en lang række forskellige
-         kompetencer indenfor industrien og vi er fremtidens førende erp virksomhed
-         i Danmark. Vores kerne teknologier er TimeXtender, Business Central og Power BI,
-         dog er dette langt fra det eneste som vi kan. Vores motto er at vi ikke arbejder for
-         dig, men med dig, da din succes er hvad vi gerne vil opnå.
+        <div>
+          <h1 className="text-center max-w-6xl font-semibold mx-auto pt-40 xxsm:text-xl basemd:text-5xl ssm:text-4xl">
+            Certificeret og erfarende konsulenter
+          </h1>
+          <p className="text-center max-w-6xl mx-auto max-w-xl pt-8 pb-10 xxsm:text-md px-5 ssm:text-xl ssm:leading-relaxed">
+            Vi er et velerfarende og certificeret team med en lang række forskellige
+            kompetencer indenfor industrien og vi er fremtidens førende erp virksomhed
+            i Danmark. Vores kerne teknologier er TimeXtender, Business Central og Power BI,
+            dog er dette langt fra det eneste som vi kan. Vores motto er at vi ikke arbejder for
+            dig, men med dig, da din succes er hvad vi gerne vil opnå.
 
 
-        </p>
-      </div>
-
-      <div className="grid aboutlg:grid-cols-4 basemd:grid-cols-3 grid-cols-2 gap-4 text-center max-w-6xl mx-auto pt-5">
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png"
-            }
-            alt="mockup"
-          />
-        </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png"
-            }
-            alt="mockup"
-          />
-        </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png"
-            }
-            alt="mockup"
-          />
-        </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png"
-            }
-            alt="mockup"
-          />
+          </p>
         </div>
 
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png"
-            }
-            alt="mockup"
-          />
+        <div className="grid aboutlg:grid-cols-4 basemd:grid-cols-3 grid-cols-2 gap-4 text-center max-w-6xl mx-auto pt-5">
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_16_dzizvl.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_17_jzijol.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_18_zck8b5.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_19_oildar.png"
+              }
+              alt="mockup"
+            />
+          </div>
+
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_20_hr5p6j.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png"
+              }
+              alt="mockup"
+            />
+          </div>
+          <div>
+            <img
+              className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
+              src={
+                "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png"
+              }
+              alt="mockup"
+            />
+          </div>
         </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_21_gydk0v.png"
-            }
-            alt="mockup"
-          />
-        </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_22_m522zl.png"
-            }
-            alt="mockup"
-          />
-        </div>
-        <div>
-          <img
-            className="xxsm:h-auto xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto"
-            src={
-              "https://res.cloudinary.com/ds8p0huok/image/upload/v1644230015/samples/Group_23_bic9fl.png"
-            }
-            alt="mockup"
-          />
-        </div>
-      </div>
       </div>
       <div className="border-b border-2 border-line mx-auto max-w-7xl w-5/6" />
     </div>
