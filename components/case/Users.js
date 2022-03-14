5bbe130f96af8efeb53d9bc5b379c36213131619
AeeUX <andreaseriksen99@hotmail.com>
2022-03-14 10:44:42 +0100

    Update Users.js

--- components/case/Users.js
+++ components/case/Users.js
@@ -18,17 +18,42 @@ export const Cabola = [
   },
 ]
 
-export const Apple = [
+export const GraderNord = [
   {
     id: 2,
-    name: 'Apple',
-    title: 'Appstore Center 365',
-    textBox1: 'Apple1 Apple1 Apple1 Apple1',
-    textBox2: 'Apple2 Apple2 Apple2 Apple2',
-    textBox3: 'Apple3 Apple3 Apple3 Apple3',
+    name: '55 Grader Nord',
+    title: 'Dynamics 365 Business Central',
+    textBox1:
+      'Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.',
+    textBox2:
+      'Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.',
+    textBox3:
+      'Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.',
+    textBox4:
+      '”Som mindre virksomhed, er det svært at skifte ERP-system uden at bruge for meget tid og ressourcer. Vi kunne derfor godt tænke os nogle erfarende udviklere og konsulenter til at skifte vores ERP system fra C5 til Dynamic 365 Business Central!',
     href: 'case',
     imageSrc:
-      'https://www.apple.com/v/home/aj/images/promos/airpods/tile_airpods__fuv6vgn42y2q_small.jpg',
-    imageAlt: 'caseOne',
+      'https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+    imageAlt: 'Cabola',
+  },
+]
+
+export const Raudahl = [
+  {
+    id: 3,
+    name: 'Raudahl & Moesby',
+    title: 'Dynamics 365 Business Central',
+    textBox1:
+      'Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.',
+    textBox2:
+      'Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.',
+    textBox3:
+      'Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.',
+    textBox4:
+      '”Som mindre virksomhed, er det svært at skifte ERP-system uden at bruge for meget tid og ressourcer. Vi kunne derfor godt tænke os nogle erfarende udviklere og konsulenter til at skifte vores ERP system fra C5 til Dynamic 365 Business Central!',
+    href: 'case',
+    imageSrc:
+      'https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+    imageAlt: 'Cabola',
   },
 ]
