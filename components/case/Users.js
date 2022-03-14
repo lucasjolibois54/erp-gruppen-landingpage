03366e0a93f4ad58ad4bd8f27407cd33cebc3bfc
daudmir <daud0013@edu.easj.dk>
2022-03-14 11:10:07 +0100

    Update Users.js

--- components/case/Users.js
+++ components/case/Users.js
@@ -4,14 +4,16 @@ export const Cabola = [
     name: 'Cabola ApS',
     title: 'Dynamics 365 Business Central',
     textBox1:
-      'Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.',
+      'Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmidler, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.',
     textBox2:
-      'Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.',
+      'Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen været en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.',
     textBox3:
       'Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.',
     textBox4:
       '”Som mindre virksomhed, er det svært at skifte ERP-system uden at bruge for meget tid og ressourcer. Vi kunne derfor godt tænke os nogle erfarende udviklere og konsulenter til at skifte vores ERP system fra C5 til Dynamic 365 Business Central!',
-    href: 'case',
+    textBox5:
+    'Cabola er blevet inspireret til at implementere endnu flere af ERPgruppens tillægsløsninger. Sammen med ERPgrupppens udvalgte konsulenter er Cabola ved at gå igang med implementeringen af Power BI, hvilket skal hjælpe dem til at bruge deres data bedst muligt.',
+      href: 'case',
     imageSrc:
       'https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
     imageAlt: 'Cabola',
