25a4f8469efcf2511fadb0f806dc391faaca3e2d
AeeUX <andreaseriksen99@hotmail.com>
2022-03-14 12:11:27 +0100

    Create Info.js

---
+++ components/menu/Info.js
@@ -0,0 +1,60 @@
+
+  
+  export const Dropdowns = [
+    {
+      id: 1,
+      name: 'Business Dynamics',
+      option1: 'Dynamics 365 Business Central',
+      option2: 'Kom godt i gang',
+      option3: 'Gratis webinar',
+      option4: 'Priser',
+      option5: 'Træning & Support',
+      option6: 'Tillægsløsninger',
+    },
+    {
+      id: 2,
+      name: 'Sales(CRM)',
+      option1: 'Dynamics 365 Sales',
+      option2: 'Kom godt i gang',
+      option3: 'Gratis webinar',
+    },
+    {
+      id: 3,
+      name: 'Power',
+      option1: 'Power Platform',
+      option2: 'Power BI',
+      option3: 'Power Apps',
+      option4: 'Power Automate',
+    },
+    {
+      id: 4,
+      name: 'Om',
+      option1: 'Hvem er ERPGruppen',
+      option2: 'Medarbejdere',
+      option3: 'Karriere',
+      option4: 'Kundereferencer',
+      option5: 'Microsoft løsninger',
+      option6: 'Samarbejdspatnere',
+      option7: 'Standardbetingelser',
+    },
+  ]
+  
+  export const Links = [
+    {
+      id: 1,
+      name: 'Træning',
+    },
+    {
+      id: 2,
+      name: 'Webinar',
+    },
+    {
+      id: 3,
+      name: 'Nyheder',
+    },
+    {
+      id: 4,
+      name: 'Kontakt',
+    },
+  ]
+  
