424ad1a6fc5e8e580afc32a6bdf767419e966eef
daudmir <daud0013@edu.easj.dk>
2022-03-14 13:00:48 +0100

    Update Users.js

--- components/case/Users.js
+++ components/case/Users.js
@@ -64,16 +64,21 @@ export const Raudahl = [
     name: 'Raudahl & Moesby',
     title: 'Dynamics 365 Business Central',
     textBox1:
-      'Cabola er en virksomhed der blev stiftet i 2004 og drives af direktør Kenneth Laursen. Kemieksperterne i Cabola tilbyder alle former for kemi; kemiske produkter, rengøringsmilder, saltprodukter og filtermaterialer. Med et stærkt team af ni medarbejdere knokler de dagligt for at sikre deres kunder den bedste løsning.',
+      'Raundahl & Moesby er en vækstvirksomhed bygget på solide og erfarne kræfter i ledelsen og blandt nøglemedarbejderne. Siden virksomheden blev stiftet i sommeren 2013 har vi opnået en stærk og synlig placering i ejendomsbranchen, med en profil som en ansvarlig, engageret og konkurrencedygtig totalentreprenør og udviklingsrådgiver.',
     textBox2:
-      'Mange virksomheder udelukker muligheden for nyt ERP-system fordi det er omkostningsdyrt, men for Cabola har beslutningen være en investering fordi det er et simpelt system der nemt kan integreres med andre systemer som virksomheden dagligt bruger. Cabola har fået implementeret Nemhandel som også er et af ERP-gruppens tillægsløsninger.',
+      'ERPgruppen har også forestået oplæring af medarbejderne hos Raundahl & Moesby. Der har blandt andet været afholdt interne undervisningsdage skræddersyet til virksomheden. “En grundig gennemgang af funktionerne var vigtig for os. På undervisningsdagene fik vi en introduktion til, hvordan det fungerede, og derefter prøvede vi selv. Det kører perfekt nu, og vi har fået mulighed for at trække de informationer, som vi ønskede os fra start. Jeg synes, vi har fået et godt system, der understøtter den vækst, vi forventer fremadrettet.”',
     textBox3:
       'Takket være ERP-løsningen og den gode support fra ERPsupportens konsulenter så har Cabola opnået en øget effektivitet i arbejdsdagen.  Cabola medarbejderne kan selv arbejde i systemet og får rigtig god support af konsulenterne som altid er til rådighed og vejleder i systemet. De nye opdateringer der kommer, kan mærkes hos Cabola,  som er velinformeret omkring dette af ERPsupportens Konsulenter.',
     textBox4:
-      '”Som mindre virksomhed, er det svært at skifte ERP-system uden at bruge for meget tid og ressourcer. Vi kunne derfor godt tænke os nogle erfarende udviklere og konsulenter til at skifte vores ERP system fra C5 til Dynamic 365 Business Central!',
+      '“Jeg har tidligere oplevet at udskiftningen af et ERP-system kunne være noget langhåret, men ERPgruppen har klaret projektledelsen rigtig fint.“',
+    heading:
+    'Fremtidige vækst',
       href: 'case',
     imageSrc:
-      'https://images.unsplash.com/photo-1561654791-00316c79efa8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+      'https://www.raundahl-moesby.dk/upload/images/Stemningsbilleder/R%20og%20M_%20188.jpg',
     imageAlt: 'Cabola',
+    imageSrc1:
+    'https://www.raundahl-moesby.dk/upload/images/tegning.jpg',
+  imageAlt1: 'lalaland',
   },
 ]
