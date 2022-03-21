88f0e85ffb974ee7d2e3ad30cb25ca4550f22dc3
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-21 12:11:04 +0100

    Create tabs.js

---
+++ components/webinar/tabs.js
@@ -0,0 +1,24 @@
+import Fade from 'react-reveal/Fade'
+
+function Tabs() {
+    return (
+
+
+        <div>
+            <div className="pt-20">
+                <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
+<div className='text-center'>
+               
+
+
+
+
+               </div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default Tabs
