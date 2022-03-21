c77744bb9f782b19e7409203f73e209d875d18ef
AeeUX <andreaseriksen99@hotmail.com>
2022-03-21 09:33:05 +0100

    Delete TagTemplate.js

--- components/tags/TagTemplate.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import Fade from 'react-reveal/Fade'
-
-function TagTemplate(props) {
-  return <div>{props.name}</div>
-}
-
-export default TagTemplate
