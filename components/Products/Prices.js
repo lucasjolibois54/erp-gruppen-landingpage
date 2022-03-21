2da9bcfcdaa4eedf969e7c0f3ea8168b08ff28df
AeeUX <andreaseriksen99@hotmail.com>
2022-03-21 13:31:58 +0100

    Update Prices.js

--- components/Products/Prices.js
+++ components/Products/Prices.js
@@ -1,183 +1,178 @@
 import Fade from 'react-reveal/Fade'
 
 function Prices() {
-    return (
+	return (
+		<div>
+			<div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl hidden lg:block ">
+				<div id="top-about" className=" p-0 flex flex-col w-full lg:flex-row md:p-0 mb-4">
+					<div classNameName="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left lg:pl-10">
+						<h2 classNameName="text-4xl sm:text-5xl md:text-7xl 2xl:text-4xl font-bold lg:mt-0 mt-4 md:mt-0 2xl:mt-4 text-dark-text py-0">
+							Priser <span className="text-4xl"> – tilkøb til Dynamics 365 Business Central</span>
+						</h2>
+						<div className="mt-10">
+							<p className="leading-loose mb-10">
+								Nedenfor kan du se en prisoversigt over Dynamics 365 Business Central.
+								I oversigten kan du ligeledes se, hvilke tilkøb du har mulighed for til Dynamics 365 Business Central,
+								eks. undervisning, support, hjælp til opsætning, konvertering og apps.
 
+							</p>
+							<p className="leading-loose mb-32">
+								Har du spørgsmål, eller ønsker du at bestille,
+								kan du få hjælp fra os på tlf. 70 70 70 66 eller support@erpsupporten.dk.
 
-        <div>
-            <div className="pt-0">
+							</p>
+						</div>
+					</div>
+				</div>
+				<div className="container">
+					<table className="text-center w-full">
+						<thead className="bg-black flex text-white w-full">
+							<tr className="flex w-full mb-4">
+								<th className="p-4 w-1/4"></th>
+								<th className="p-4 w-1/4">Basic</th>
+								<th className="p-4 w-1/4">Essential</th>
+								<th className="p-4 w-1/4">Premium</th>
+							</tr>
+						</thead>
+						<tbody className="flex flex-col items-center justify-between w-full">
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100 text-2xl my-auto py-10">Cloud - leje</td>
+								<td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 157,30 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
+								<td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 440,50 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
+								<td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 629,30 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Team Member (Cloud - leje) <br /> <span className="text-xs">(begrænset adgang)</span></td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">kr. 50,30 <br /> <span className="text-xs">pr. måned pr. bruger</span></td>
+								<td className="p-4 w-1/4">kr. 50,30 <br /> <span className="text-xs">pr. måned pr. bruger</span></td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4">
+								<td className="p-4 w-1/4">Dogs</td>
+								<td className="p-4 w-1/4">Cats</td>
+								<td className="p-4 w-1/4">Birds</td>
+								<td className="p-4 w-1/4">Fish</td>
+							</tr>
+							<tr className="flex w-full mb-4 divide-white divide-x-8">
+								<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
+								<td className="p-4 w-1/4 bg-gray-100">Cats</td>
+								<td className="p-4 w-1/4 bg-gray-100">Birds</td>
+								<td className="p-4 w-1/4 bg-gray-100">Fish</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 
-            </div>
-            <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
-                <div id="top-about" className=" p-0 flex flex-col w-full lg:flex-row md:p-0 mb-4">
-                    <div classNameName="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left lg:pl-10">
-                        <h2 classNameName="text-4xl sm:text-5xl md:text-7xl 2xl:text-4xl font-bold lg:mt-0 mt-4 md:mt-0 2xl:mt-4 text-dark-text py-0">
-                        Priser <span className="text-4xl"> – tilkøb til Dynamics 365 Business Central</span>
-                        </h2>
-                        <div className="mt-10">
-                    <p className="leading-loose mb-10">
-                    Nedenfor kan du se en prisoversigt over Dynamics 365 Business Central. 
-                    I oversigten kan du ligeledes se, hvilke tilkøb du har mulighed for til Dynamics 365 Business Central, 
-                    eks. undervisning, support, hjælp til opsætning, konvertering og apps.
-
-                    </p>
-
-                    <p className="leading-loose mb-32">
-                    Har du spørgsmål, eller ønsker du at bestille, 
-                    kan du få hjælp fra os på tlf. 70 70 70 66 eller support@erpsupporten.dk.
-
-                    </p>
-                    </div>
-                        
-                    </div>
-                    
-                </div>
-
-<div className="container">
-	<table className="text-center w-full">
-		<thead className="bg-black flex text-white w-full">
-			<tr className="flex w-full mb-4">
-				<th className="p-4 w-1/4"></th>
-				<th className="p-4 w-1/4">Basic</th>
-				<th className="p-4 w-1/4">Essential</th>
-				<th className="p-4 w-1/4">Premium</th>
-			</tr>
-		</thead>
-		<tbody className="flex flex-col items-center justify-between w-full">
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100 text-2xl my-auto py-10">Cloud - leje</td>
-				<td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 157,30 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
-				<td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 440,50 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
-                <td className="p-4 w-1/4 text-4xl bg-gray-100">kr. 629,30 <br /> <span className="text-sm">pr. måned pr. bruger</span></td>
-			</tr>
-			<tr className="flex w-full mb-4">
-            <td className="p-4 w-1/4">Team Member (Cloud - leje) <br /> <span className="text-xs">(begrænset adgang)</span></td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">kr. 50,30 <br /> <span className="text-xs">pr. måned pr. bruger</span></td>
-                <td className="p-4 w-1/4">kr. 50,30 <br /> <span className="text-xs">pr. måned pr. bruger</span></td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4">
-				<td className="p-4 w-1/4">Dogs</td>
-				<td className="p-4 w-1/4">Cats</td>
-				<td className="p-4 w-1/4">Birds</td>
-				<td className="p-4 w-1/4">Fish</td>
-			</tr>
-			<tr className="flex w-full mb-4 divide-white divide-x-8">
-				<td className="p-4 w-1/4 bg-gray-100">Dogs</td>
-				<td className="p-4 w-1/4 bg-gray-100">Cats</td>
-				<td className="p-4 w-1/4 bg-gray-100">Birds</td>
-				<td className="p-4 w-1/4 bg-gray-100">Fish</td>
-			</tr>
-		</tbody>
-	</table>
-</div>
-                
-
-            </div>
-        </div>
-    )
+			{/* Small mobile pricings */}
+			
+			<div className='block lg:hidden'>
+				<h2>I am small!</h2>
+			</div>
+		</div>
+	)
 
 }
 
