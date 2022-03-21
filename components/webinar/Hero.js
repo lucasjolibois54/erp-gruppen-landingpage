747900875f5d7be24ca3e6565b1bd935bff3930f
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-21 14:40:10 +0100

    Update Hero.js

--- components/webinar/Hero.js
+++ components/webinar/Hero.js
@@ -9,10 +9,10 @@ function Hero() {
                 <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
 <div className='text-center'>
                     <h3 className='text-lg sm:text-xl md:text-xl 2xl:text-2xl font-bold text-gray-two'>ERP Gruppen</h3>
-                    <h1 className='text-4xl sm:text-5xl md:text-7xl 2xl:text-8xl font-bold mt-5 md:mt-10'>
+                    <h1 className='text-4xl sm:text-5xl md:text-7xl 2xl:text-8xl font-bold mt-10'>
                         Gratis webinar
                     </h1>
-                    <p className='max-w-5xl mx-auto text-base md:text-lg mt-7 font-light ml-7 mr-7'>Tag på gratis webinar og bliv klogere på mulighederne med Microsoft Dynamics 365 løsninger.
+                    <p className='max-w-5xl mx-auto text-base md:text-lg mt-7 font-light pl-7 pr-7 '>Tag på gratis webinar og bliv klogere på mulighederne med Microsoft Dynamics 365 løsninger.
                         Du kan få en grundlæggende introduktion til forretningsløsningerne Dynamics 365 Business Central, D365 Ejendom eller Dynamics 365 Sales.
                     </p>
                     </div>
