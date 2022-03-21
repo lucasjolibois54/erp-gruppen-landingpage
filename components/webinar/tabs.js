fde862ab6ac40f79c44e0c2952fdd304863f54dd
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-21 14:40:12 +0100

    Update tabs.js

--- components/webinar/tabs.js
+++ components/webinar/tabs.js
@@ -14,7 +14,7 @@ function Tabs() {
                         }}
                     >
                         <Tab.List>
-                            <div className='text-center space-x-5'>
+                            <div className='text-center xs:space-x-5 md:space-x-0 space-y-5'>
                                 <Tab><div className='max-w-xs border-2 pr-8 pl-8 pt-4 pb-4 rounded-lg hover:border-blue-500 hover:bg-blue-50 transition-all ease-in-out'><h2 className='text-left text-2xl font-bold text-gray-two'>Business central</h2> <p className='text-left text-sm text-gray-two'>Find et BC webinar og få et indbliki økonomi og forretningsstyring</p></div></Tab>
                                 <Tab><div className='max-w-xs border-2 pr-8 pl-8 pt-4 pb-4 rounded-lg hover:border-blue-500 hover:bg-blue-50 transition-all ease-in-out'><h2 className='text-left text-2xl font-bold text-gray-two'>Sales (CRM)</h2> <p className='text-left text-sm text-gray-two'>Find et sales webinar of få et bedre indblik omkring økonomistyring</p></div></Tab>
                                 <Tab><div className='max-w-xs border-2 pr-8 pl-8 pt-4 pb-4 rounded-lg hover:border-blue-500 hover:bg-blue-50 transition-all ease-in-out'><h2 className='text-left text-2xl font-bold text-gray-two'>D365 Ejendom</h2> <p className='text-left text-sm text-gray-two'>Find et D365 ejendom webinar og få indblik i ejendoms administration</p></div></Tab>
@@ -23,14 +23,14 @@ function Tabs() {
                         <Tab.Panels>
                             <Tab.Panel> <div className='text-center mt-28'>
 
-                                <img className="max-w-6xl xxsm:h-auto w-5/6 my-auto mt-20 md:mt-10 2xl:mt-16 mb-20 mx-auto"
+                                <img className="max-w-6xl xxsm:h-auto w-5/6 my-auto mt-20 md:mt-10 2xl:mt-16 mb-0 mx-auto"
                                     src={
                                         " https://res.cloudinary.com/dckwf6med/image/upload/v1647863695/erp/businesscentral-banner_f4tpqk.webp"
                                     } alt="Banner" />
 
 
                                 <div>
-                                    <div className=" pb-10 lg:pb-36 xsm:pl-50 xsm:pr-20 pl-5 pr-5 md:pl-36 md:pr-36 lg:pl-10 lg:pr-10 ">
+                                    <div className=" pb-10 lg:pb-36 xsm:pl-10 xsm:pr-10 pr-5 md:pl-36 md:pr-36 lg:pl-10 lg:pr-10 ">
 
                                         <div className="font-sans mb-0 pt-20 flex flex-col content-center justify-center justify-items-center text-off-white text-3xl md:flex-row bg-transparent">
                                             <div className="w-full  pt-0 pb-20 pr-0 text-center w-100% lg:pr-10 md:w-7/12 lg:w-5/12 lg:mr-0 md:mr-10">
