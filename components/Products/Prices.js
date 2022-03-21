04a429d6e06c81b19c22bffb13416f2f2354ac33
daudmir <daud0013@edu.easj.dk>
2022-03-21 12:24:49 +0100

    Create Prices.js

---
+++ components/Products/Prices.js
@@ -0,0 +1,43 @@
+import Fade from 'react-reveal/Fade'
+
+function Prices() {
+    return (
+
+
+        <div>
+            <div className="pt-0">
+
+            </div>
+            <div className="my-auto mx-auto xxsm:mt-6 lgbasemd:mt-0 max-w-7xl">
+                <div id="top-about" className=" p-0 flex flex-col w-full lg:flex-row md:p-0 mb-4">
+                    <div className="flex text-center flex-col w-full px-3 justify-center lg:w-5/10 lg:text-left lg:pl-10">
+                        <h2 className="text-4xl sm:text-5xl md:text-7xl 2xl:text-8xl font-bold lg:mt-0 mt-4 md:mt-0 2xl:mt-4 text-dark-text py-0">
+                        Priser <span className="text-4xl"> – tilkøb til Dynamics 365 Business Central</span>
+                        </h2>
+                        <div className="mt-10">
+                    <p className="leading-loose mb-10">
+                    Nedenfor kan du se en prisoversigt over Dynamics 365 Business Central. 
+                    I oversigten kan du ligeledes se, hvilke tilkøb du har mulighed for til Dynamics 365 Business Central, 
+                    eks. undervisning, support, hjælp til opsætning, konvertering og apps.
+
+                    </p>
+
+                    <p className="leading-loose mb-32">
+                    Har du spørgsmål, eller ønsker du at bestille, 
+                    kan du få hjælp fra os på tlf. 70 70 70 66 eller support@erpsupporten.dk.
+
+                    </p>
+                    </div>
+                        
+                    </div>
+                    
+                </div>
+                
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Prices
