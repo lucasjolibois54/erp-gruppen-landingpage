cd6dd4606aa1920e26c639200f7cbf535ecd65fd
AeeUX <andreaseriksen99@hotmail.com>
2022-03-22 12:48:24 +0100

    Update Footer.js

---
+++ components/policy/Policy.js
@@ -0,0 +1,26 @@
+export const Privacy = [
+    {
+        id: 1,
+        name: 'Privacy Policy',
+        title: 'Privacy Policy',
+        href: 'case',
+    },
+]
+
+export const ToP = [
+    {
+        id: 1,
+        name: 'Terms of Purchase',
+        title: 'Terms of Purchase',
+        href: 'case',
+    },
+]
+
+export const ToT = [
+    {
+        id: 1,
+        name: 'Terms of Trade',
+        title: 'Terms of Trade',
+        href: 'case',
+    },
+]
