5ed4767470f5b15fa162432f95eb131e931b436e
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-22 12:17:53 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,36 @@
 function Footer() {
   return (
     <div>
-      <div className="pt-20 pb-6">
-        <div id="footer" className="max-w-7xl mx-auto">
-          <p className=" text-lg xxsm:text-base xsm:text-xl basesm:text-xl mdsm:text-xl basemd:text-2xl baselg:text-2xl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-xl font-medium mt-56 leading-loose">
-            ERPgruppens Produktions facilitet udvikler og leverer
-            brugerdefineret services, produkter. Support fra vores certificerede
-            konsulenter.
-          </p>
-        </div>
-        <p className="text-lg xxsm:text-base xsm:text-lg basesm:text-lg mdsm:text-lg basemd:text-xl baselg:text-4xl text-green-hl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-7xl mx-auto mt-32 underline text-red-400 font-semibold">
-          <a href="mailto:">mail@erpgruppen.dk</a>
-        </p>
-        <p className="text-lg xxsm:text-base xsm:text-lg basesm:text-lg mdsm:text-lg basemd:text-xl baselg:text-4xl text-green-hl basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-left max-w-7xl mx-auto mt-4">
-          <a href="tel:004578794040">+45 78 79 40 40</a>
-        </p>
+      <div className="bg-gray-100 pt-5 m-10 ml-10 mr-10 mx-auto">
+        <div className=" px-4 sm:px-6 text-gray-800 sm:grid md:grid-cols-3 sm:grid-cols-2 mx-auto max-w-6xl">
 
-        <p className=" text-lg xxsm:text-sm xsm:text-base basesm:text-base mdsm:text-lg basemd:text-lg baselg:text-lg basemd:px-24 sbasesm:px-20 smdsm:px-20 mdsm:px-10 baselg:px-0 ssm:px-10 px-4 xsbasesm:px-16 text-right max-w-7xl mx-auto font-medium mt-36">
-          <a>© 2022-2022 ERP Gruppen, A/S</a>
-        </p>
+          <div className="p-5">
+            <div className="text-sm uppercase text-black font-bold">ERPGruppen</div>
+            <a className="my-3 block" href="/#">Essen 9 <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">6000 Kolding <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">CVR 30 82 49 46</a>
+            <div className="flex"><a className="my-3 block pr-4" href="/#"><img className=""
+              src={
+                " https://res.cloudinary.com/dckwf6med/image/upload/v1647947332/erp/Vector_hrsdpc.svg"
+              } alt="Banner" />   <span className="text-teal-600 text-xs p-1"></span></a>
+
+              <a className="my-3 block" href="/#"><img className=""
+                src={
+                  " https://res.cloudinary.com/dckwf6med/image/upload/v1647947332/erp/Vector-1_bm1dl1.svg"
+                } alt="Banner" />   <span className="text-teal-600 text-xs p-1"></span></a></div>
+          </div>
+          <div className="p-5">
+            <div className="text-sm uppercase text-black font-bold">ERPGruppen</div>
+            <a className="my-3 block" href="/#">Hjem <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">Produkter <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">Webinar <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">Om os <span className="text-teal-600 text-xs p-1"></span></a> <a className="my-3 block" href="/#">Kontakt <span className="text-teal-600 text-xs p-1"></span></a>
+          </div>
+          <div className="p-5">
+            <div className="text-sm uppercase text-black font-bold">Support</div>
+            <a className="my-3 block" href="/#">ERP Supporten <span className="text-teal-600 text-xs p-1"></span></a><a className="my-3 block" href="/#">mail@erpgruppen.dk <span className="text-teal-600 text-xs p-1"></span></a> <a className="my-3 block" href="/#">+45 78 79 40 40 <span className="text-teal-600 text-xs p-1"></span></a>
+          </div>
+        </div>
+        <div className="bg-gray-100 w-full" >
+          <p classNameName=" bg-gray-100 w-full text-right p-3 mb-5">© 2008-2022 ERPGruppen A/S</p></div>
       </div>
     </div>
+
   );
 }
 
