87544831ac69013bcbf24bf11135534a51b20b2b
daudmir <daud0013@edu.easj.dk>
2022-03-23 15:26:09 +0100

    Update Hero.js

--- components/about/Hero.js
+++ components/about/Hero.js
@@ -1,5 +1,3 @@
-import Fade from 'react-reveal/Fade'
-
 function Hero() {
     return (
 
@@ -14,12 +12,11 @@ function Hero() {
                             <strong className="font-semibold"> Mød vores team af eksperter i </strong> Marketing & Produktion
                         </h2>
                         <p className="text-base md:text-lg px-5 text-dark-text py-5 md:px-24 lg:pl-0 lg:pr-20 max-w-md text-text-light-gray mt-5">
-                            <Fade> ERP- produktion er et af de største erp virksomheder i danmark med ekspert kompetenser inden for industrien. Scroll for at lære os bedre at kende samt hvilke teknologier vi arbejder med.</Fade>
+                        ERP- produktion er et af de største erp virksomheder i danmark med ekspert kompetenser inden for industrien. Scroll for at lære os bedre at kende samt hvilke teknologier vi arbejder med.
 
                         </p>
                     </div>
                     <div className="w-full  xl:w-5/6">
-                        <Fade>
                             <img
                                 className="xxsm:h-auto xxsm:px-10 xsm:h-auto mdsm:h-auto mdsm:h-auto basemd:h-auto baselg:h-auto w-auto my-auto mt-20 md:mt-10 2xl:mt-16 mb-20 mx-auto"
                                 src={
@@ -27,7 +24,6 @@ function Hero() {
                                 }
                                 alt="mockup"
                             />
-                        </Fade>
 
                     </div>
                 </div>
