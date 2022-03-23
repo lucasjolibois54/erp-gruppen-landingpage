22cf09590add3521aca334a50f2be23cea4c4960
lucasjolibois54 <lucasjolibois@icloud.com>
2022-03-23 15:25:29 +0100

    Update CaseTemplate.js

--- components/case/CaseTemplate.js
+++ components/case/CaseTemplate.js
@@ -1,6 +1,5 @@
 //Components
 import Modal from '../modals/Modal'
-import Fade from 'react-reveal/Fade'
 
 function Info(props) {
   return (
@@ -10,7 +9,7 @@ function Info(props) {
           <div className="w-full min-h-80 aspect-w-1 p-5 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none">
             <div className="flex">
               <h3 className="text-5xl flex font-semibold">
-                <Fade top>{props.name}</Fade>
+                {props.name}
               </h3>
               <Modal />
             </div>
@@ -18,65 +17,65 @@ function Info(props) {
               className="text-gray-300 w-96 mt-5 text-4xl font-regular flex"
               aria-hidden="true"
             >
-              <Fade bottom>{props.title}</Fade>
+              {props.title}
             </span>
           </div>
           <div className="w-full min-h-80 aspect-w-1 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none flex">
             <h3 className="font-regular m-5 text-lg">
-              <Fade bottom cascade>
+             
                 {props.textBox1}
-              </Fade>
+              
             </h3>
           </div>
         </div>
-        <Fade>
+       
           <img
             className="w-full h-1/3 object-center object-cover lg:w-full lg:h-1/4 mt-20"
             src={props.imageSrc}
             alt={props.imageAlt}
           />
-        </Fade>
+        
         <div className="w-full aspect-w-1 aspect-h-1 overflow-hidden lg:h-full lg:aspect-none">
         <div className="mt-20 mx-12 text-2xl font-semi-bold">
         {props.textBox4}
         </div>
         <div className="mt-10 mx-12">
         <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox5}</Fade>
+             {props.textBox5}
             </h3>
         
-            <Fade>
+           
           <img
             className="w-full h-1/3 object-center object-cover lg:w-12/12 lg:h-1/4 mt-20"
             src={props.imageSrc1}
             alt={props.imageAlt}
           />
-        </Fade></div>
+        </div>
           <div className="mt-20 mx-12">
-            <Fade>
+           
               <h3 className="text-5xl flex font-semibold">
                 Nye implementeringer
               </h3>
-            </Fade>
+            
             <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox3}</Fade>
+             {props.textBox3}
             </h3>
             <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox6}</Fade>
+             {props.textBox6}
             </h3>
             <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox7}</Fade>
+             {props.textBox7}
             </h3>
-            <Fade>
+           
               <h3 className="text-5xl flex font-semibold mt-20">
-              <Fade bottom>{props.heading}</Fade>
+             {props.heading}
               </h3>
-            </Fade>
+            
             <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox2}</Fade>
+             {props.textBox2}
             </h3>
             <h3 className="mt-8 font-regular text-lg gri">
-              <Fade bottom>{props.textBox10}</Fade>
+             {props.textBox10}
             </h3>
           </div>
         </div>
