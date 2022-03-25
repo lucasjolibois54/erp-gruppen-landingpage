7ca24491b90bb5f837ba9c5277580ad4f8f08076
AeeUX <andreaseriksen99@hotmail.com>
2022-03-25 11:36:33 +0100

    Update Policy.js

--- components/policy/Policy.js
+++ components/policy/Policy.js
@@ -1,26 +1,17 @@
-export const Privacy = [
+export const betingelser = [
     {
         id: 1,
-        name: 'Privacy Policy',
-        title: 'Privacy Policy',
+        name: 'Standard Betingelser',
+        title: 'Standard Betingelser',
         href: 'case',
     },
 ]
 
-export const ToP = [
+export const politik = [
     {
         id: 1,
-        name: 'Terms of Purchase',
-        title: 'Terms of Purchase',
-        href: 'case',
-    },
-]
-
-export const ToT = [
-    {
-        id: 1,
-        name: 'Terms of Trade',
-        title: 'Terms of Trade',
+        name: 'Data Politik',
+        title: 'Data Politik',
         href: 'case',
     },
 ]
